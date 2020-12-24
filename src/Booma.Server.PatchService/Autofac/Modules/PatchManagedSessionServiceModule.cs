b2333d284aa9d22b45a619a7da9434deb6fd15f1
HelloKitty <andrew.blakely@ymail.com>
2020-12-24 04:11:58 -0600

    Added PatchManagedSessionServiceModule

---
+++ src/Booma.Server.PatchService/Autofac/Modules/PatchManagedSessionServiceModule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+
+namespace Booma
+{
+	/// <summary>
+	/// Patch service managed session service module.
+	/// Registers <see cref="BoomaPatchManagedSession"/> and related services.
+	/// </summary>
+	public sealed class PatchManagedSessionServiceModule : Module
+	{
+		/// <inheritdoc />
+		protected override void Load(ContainerBuilder builder)
+		{
+			base.Load(builder);
+
+			//Obviously we need a FRESH session per lifetime.
+			builder.RegisterType<BoomaPatchManagedSession>()
+				.InstancePerLifetimeScope();
+
+			//Should be stateless, so we should be alright.
+			builder.RegisterType<PatchManagedSessionFactory>()
+				.AsImplementedInterfaces()
+				.SingleInstance();
+		}
+	}
+}
