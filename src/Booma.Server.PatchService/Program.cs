b1c32918862b4dff9b007e132d57d4eeb4c701e1
HelloKitty <andrew.blakely@ymail.com>
2020-12-24 02:18:01 -0600

    Added Program to Patch Service

---
+++ src/Booma.Server.PatchService/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Common.Logging;
+using GladNet;
+
+namespace Booma
+{
+	class Program
+	{
+		public static IPAddress Address { get; } = IPAddress.Parse("127.0.0.1");
+
+		static async Task Main(string[] args)
+		{
+			ILog logger = new ConsoleLogger(LogLevel.All, true);
+			logger.Info($"Starting Patch Service.");
+
+			await new BoomaPatchServerApplication(new NetworkAddressInfo(Address, 11000), logger)
+				.BeginListeningAsync();
+		}
+	}
+}
