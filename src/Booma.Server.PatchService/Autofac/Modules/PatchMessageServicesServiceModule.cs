870987837da0a0ba8d10c67c9f4999c75fb7f1c0
HelloKitty <andrew.blakely@ymail.com>
2020-12-24 04:00:37 -0600

    Added PatchMessageHandlerServiceModule

---
+++ src/Booma.Server.PatchService/Autofac/Modules/PatchMessageServicesServiceModule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using Booma.Proxy;
+using GladNet;
+
+namespace Booma
+{
+	/// <summary>
+	/// Patch service message services (dependencies involved in messages) service module.
+	/// </summary>
+	public sealed class PatchMessageServicesServiceModule : Module
+	{
+		/// <inheritdoc />
+		protected override void Load(ContainerBuilder builder)
+		{
+			base.Load(builder);
+
+			//This is just message serialization stuff. We don't need to make this instance per, it should be stateless.
+			builder.RegisterType<SessionMessageBuildingServiceContext<PSOBBPatchPacketPayloadClient, PSOBBPatchPacketPayloadServer>>()
+				.AsSelf()
+				.SingleInstance();
+
+			builder.RegisterType<InPlaceNetworkMessageDispatchingStrategy<PSOBBPatchPacketPayloadClient, PSOBBPatchPacketPayloadServer>>()
+				.AsImplementedInterfaces()
+				.InstancePerLifetimeScope();
+		}
+	}
+}
