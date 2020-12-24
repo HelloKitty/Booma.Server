5151a6ee51420136754d16ab4e9760114235895d
HelloKitty <andrew.blakely@ymail.com>
2020-12-24 03:54:04 -0600

    Added DefaultLoggingServiceModule

---
+++ src/Booma.Server.PatchService/Autofac/Modules/DefaultLoggingServiceModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using Common.Logging;
+
+namespace Booma
+{
+	/// <summary>
+	/// Default logging module that registers <see cref="ILog"/>.
+	/// </summary>
+	public sealed class DefaultLoggingServiceModule : Module
+	{
+		protected override void Load(ContainerBuilder builder)
+		{
+			base.Load(builder);
+
+			ILog logger = new ConsoleLogger(LogLevel.All, true);
+
+			builder.RegisterInstance(logger)
+				.As<ILog>()
+				.SingleInstance();
+		}
+	}
+}
