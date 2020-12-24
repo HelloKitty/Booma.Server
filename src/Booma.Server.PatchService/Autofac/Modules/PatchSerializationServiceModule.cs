a8c30870dba53a8fab181184b29e06f1b411c3a2
HelloKitty <andrew.blakely@ymail.com>
2020-12-24 04:00:45 -0600

    Added PatchSerializationServiceModule

---
+++ src/Booma.Server.PatchService/Autofac/Modules/PatchSerializationServiceModule.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+
+namespace Booma
+{
+	/// <summary>
+	/// Patch service packet/header serialization service module.
+	/// </summary>
+	public sealed class PatchSerializationServiceModule : Module
+	{
+		protected override void Load(ContainerBuilder builder)
+		{
+			base.Load(builder);
+
+			//All the patch serializer stuff is stateless and can be shared
+			//so we're going to SingleInstance it.
+			builder
+				.RegisterType<PatchPacketSerializer>()
+				.AsImplementedInterfaces()
+				.SingleInstance();
+
+			builder.RegisterType<PatchPacketHeaderFactory>()
+				.AsImplementedInterfaces()
+				.SingleInstance();
+
+			builder.RegisterType<PatchPacketHeaderSerializer>()
+				.AsImplementedInterfaces()
+				.SingleInstance();
+		}
+	}
+}
