4a767e44172677ff59096dbe2ffa14aa0769f206
HelloKitty <andrew.blakely@ymail.com>
2020-12-24 02:07:55 -0600

    Added PatchPacketSerializer

---
+++ src/Booma.Server.PatchService/Network/Serialization/PatchPacketSerializer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Booma.Proxy;
+using FreecraftCore.Serializer;
+using GladNet;
+using JetBrains.Annotations;
+
+namespace Booma
+{
+	/// <summary>
+	/// GladNet <see cref="IMessageSerializer{TMessageType}"/> and <see cref="IMessageDeserializer{TMessageType}"/> adapter
+	/// around Booma's FreecraftCore <see cref="SerializerService"/>. Providing serialization for <see cref="PSOBBPatchPacketPayloadServer"/> and
+	/// <see cref="PSOBBPatchPacketPayloadClient"/>.
+	/// </summary>
+	public sealed class PatchPacketSerializer : IMessageSerializer<PSOBBPatchPacketPayloadServer>, IMessageDeserializer<PSOBBPatchPacketPayloadClient>
+	{
+		//TODO: Inject
+		/// <summary>
+		/// The adapted serialization service.
+		/// </summary>
+		private SerializerService Serializer { get; } = new SerializerService();
+
+		public PatchPacketSerializer()
+		{
+			Serializer.RegisterPatchPacketSerializers();
+		}
+
+		/// <inheritdoc />
+		public void Serialize([NotNull] PSOBBPatchPacketPayloadServer value, Span<byte> buffer, ref int offset)
+		{
+			if (value == null) throw new ArgumentNullException(nameof(value));
+
+			Serializer.Write(value, buffer, ref offset);
+		}
+
+		/// <inheritdoc />
+		public PSOBBPatchPacketPayloadClient Deserialize(Span<byte> buffer, ref int offset)
+		{
+			return Serializer.Read<PSOBBPatchPacketPayloadClient>(buffer, ref offset);
+		}
+	}
+}
