04c04552b3b36f85a77f01f299437f06f47fae71
HelloKitty <andrew.blakely@ymail.com>
2020-12-24 20:18:52 -0600

    Refactored PatchMessageHandler module to use ServerMessageHandlerServiceModule

--- src/Booma.Server.PatchService/Autofac/Modules/PatchMessageHandlerServiceModule.cs
+++ src/Booma.Server.PatchService/Autofac/Modules/PatchMessageHandlerServiceModule.cs
@@ -12,24 +12,8 @@ namespace Booma
 	/// Patch service message handler service module.
 	/// Registers the services and handling for message handlers.
 	/// </summary>
-	public sealed class PatchMessageHandlerServiceModule : Module
+	public sealed class PatchMessageHandlerServiceModule : ServerMessageHandlerServiceModule<PSOBBPatchPacketPayloadClient, PSOBBPatchPacketPayloadServer, DefaultPatchMessageHandler>
 	{
-		/// <inheritdoc />
-		protected override void Load(ContainerBuilder builder)
-		{
-			base.Load(builder);
 
-			//Handlers AND handler service should be stateless so we only need single instance.
-			builder
-				.RegisterType<DefaultMessageHandlerService<PSOBBPatchPacketPayloadClient, SessionMessageContext<PSOBBPatchPacketPayloadServer>>>()
-				.As<IMessageHandlerService<PSOBBPatchPacketPayloadClient, SessionMessageContext<PSOBBPatchPacketPayloadServer>>>()
-				.OnActivated(args =>
-				{
-					//TODO: Autodiscovery handlers via reflection.
-					//Bind one of the default handlers
-					args.Instance.Bind<PSOBBPatchPacketPayloadClient>(new DefaultPatchMessageHandler(args.Context.Resolve<ILog>()));
-				})
-				.SingleInstance();
-		}
 	}
 }
