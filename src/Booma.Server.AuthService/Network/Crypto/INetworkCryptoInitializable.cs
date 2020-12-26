0b0965abee0c159c0052d42518d81eadb3b75455
HelloKitty <andrew.blakely@ymail.com>
2020-12-25 21:04:30 -0600

    Added Crypto interfaces

---
+++ src/Booma.Server.AuthService/Network/Crypto/INetworkCryptoInitializable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Booma
+{
+	/// <summary>
+	/// Contract for service that provides network (double-sided) encryption initializables.
+	/// </summary>
+	public interface INetworkCryptoInitializable
+	{
+		/// <summary>
+		/// The encryption initializer for (out-going) crypto provider.
+		/// </summary>
+		ICryptoKeyInitializable<byte[]> EncryptionInitializable { get; }
+
+		/// <summary>
+		/// The decryption initializer for (in-coming) crypto provider.
+		/// </summary>
+		ICryptoKeyInitializable<byte[]> DecryptionInitializable { get; }
+	}
+}
---
+++ src/Booma.Server.AuthService/Network/Crypto/INetworkCryptoService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Booma
+{
+	/// <summary>
+	/// Contract for service that provides network (double-sided) encryption.
+	/// </summary>
+	public interface INetworkCryptoService
+	{
+		/// <summary>
+		/// The encryption (out-going) crypto provider.
+		/// </summary>
+		ICryptoServiceProvider EncryptionProvider { get; }
+
+		/// <summary>
+		/// The decryption (in-coming) crypto provider.
+		/// </summary>
+		ICryptoServiceProvider DecryptionProvider { get; }
+	}
+}
