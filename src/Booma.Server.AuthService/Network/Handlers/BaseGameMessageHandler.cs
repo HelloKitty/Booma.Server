134772034f35e3a63034e1d328b6196d9a0fefc7
HelloKitty <andrew.blakely@ymail.com>
2020-12-25 23:59:26 -0600

    Added BaseGameMessageHandler

---
+++ src/Booma.Server.AuthService/Network/Handlers/BaseGameMessageHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Booma.Proxy;
+using GladNet;
+
+namespace Booma
+{
+	/// <summary>
+	/// Base <see cref="BaseSpecificMessageHandler{TMessageType,TBaseMessageType,TMessageContext}"/> for <see cref="PSOBBGamePacketPayloadClient"/>s.
+	/// </summary>
+	/// <typeparam name="TMessageType">The specific client message.</typeparam>
+	public abstract class BaseGameMessageHandler<TMessageType> : BaseSpecificMessageHandler<TMessageType, PSOBBGamePacketPayloadClient, SessionMessageContext<PSOBBGamePacketPayloadServer>> 
+		where TMessageType : PSOBBGamePacketPayloadClient
+	{
+
+	}
+}
