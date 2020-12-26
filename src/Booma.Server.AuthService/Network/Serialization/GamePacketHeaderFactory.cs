decbd35a722290b41ddb9ce59bb083ba83974091
HelloKitty <andrew.blakely@ymail.com>
2020-12-25 21:44:46 -0600

    Added Game Packet Header Services

---
+++ src/Booma.Server.AuthService/Network/Serialization/GamePacketHeaderFactory.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Booma.Proxy;
+using GladNet;
+
+namespace Booma
+{
+	/// <summary>
+	/// GladNet <see cref="IPacketHeaderFactory"/> PSO Booma Game Server implementation.
+	/// </summary>
+	public sealed class GamePacketHeaderFactory : IPacketHeaderFactory
+	{
+		private NetworkConnectionOptions Options { get; }
+
+		public GamePacketHeaderFactory([NotNull] NetworkConnectionOptions options)
+		{
+			Options = options ?? throw new ArgumentNullException(nameof(options));
+		}
+
+		/// <inheritdoc />
+		public IPacketHeader Create(PacketHeaderCreationContext context)
+		{
+			//TODO: We should deserialize a PSOBB Game Header DTO instead.
+			short size = Unsafe.As<byte, short>(ref context.GetSpan()[0]);
+
+			//PSOBB has size as FULL packet size. PSOBBPacketHeader object will provide
+			//correct payload vs packet lengths.
+			return new PPSOBBHeaderAdapter(new PSOBBPacketHeader(size));
+		}
+
+		/// <inheritdoc />
+		public bool IsHeaderReadable(in Span<byte> buffer)
+		{
+			//8 is cipher blocks size.
+			return buffer.Length >= 8;
+		}
+
+		/// <inheritdoc />
+		public bool IsHeaderReadable(in ReadOnlySequence<byte> buffer)
+		{
+			//8 is cipher blocks size.
+			return buffer.Length >= 8;
+		}
+
+		/// <inheritdoc />
+		public int ComputeHeaderSize(in ReadOnlySequence<byte> buffer)
+		{
+			return 8;
+		}
+	}
+}
