5b5b41203c7c7b0b3de8792fcaa038c8431fffb1
HelloKitty <andrew.blakely@ymail.com>
2020-12-26 04:04:10 -0600

    Added Booma.Server.ServiceDiscoveryService Application
    Base on my new SD library:
    https://github.com/HelloKitty/Glader.ASP.ServiceDiscovery

---
+++ src/Booma.Server.ServiceDiscoveryService/Startup.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Glader.ASP.ServiceDiscovery;
+using Microsoft.EntityFrameworkCore;
+
+namespace Booma
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			//Just add the SD controller
+			services.AddControllers()
+				.RegisterServiceDiscoveryController();
+
+			//And this is the SD database and database services.
+			services.RegisterServiceDiscoveryDatabase(builder =>
+			{
+				builder.UseMySql("server=127.0.0.1;port=3306;Database=guardians.global;Uid=root;Pwd=test;");
+			});
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if(env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseHttpsRedirection();
+
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+			});
+		}
+	}
+}
