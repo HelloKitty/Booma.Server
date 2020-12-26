cd3c2644b5e358af166bd323e94ce09541188222
HelloKitty <andrew.blakely@ymail.com>
2020-12-25 21:04:15 -0600

    Added GameSerializationServiceModule

---
+++ src/Booma.Server.AuthService/Autofac/Modules/GameSerializationServiceModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using Autofac.Core;
+using Booma.Proxy;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+
+namespace Booma
+{
+	/// <summary>
+	/// Game service packet/header serialization service module.
+	/// </summary>
+	public sealed class GameSerializationServiceModule 
+		: ServerMessageSerializationServiceModule<PSOBBGamePacketPayloadClient, PSOBBGamePacketPayloadServer, GamePacketHeaderFactory, GamePacketHeaderSerializer>
+	{
+		protected override void OnSerializerCreated(IActivatedEventArgs<SerializerService> serializerActivatedEventArgs)
+		{
+			if (serializerActivatedEventArgs == null) throw new ArgumentNullException(nameof(serializerActivatedEventArgs));
+
+			//Only need to register the game serializers.
+			serializerActivatedEventArgs.Instance.RegisterGamePacketSerializers();
+		}
+	}
+}
