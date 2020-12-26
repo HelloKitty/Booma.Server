256d18b3cf4a47262c2429dbba77aec129a01a4c
HelloKitty <andrew.blakely@ymail.com>
2020-12-25 21:44:21 -0600

    Added PPSOBBHeaderAdapter

---
+++ src/Booma.Server.AuthService/Network/PPSOBBHeaderAdapter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Booma.Proxy;
+using GladNet;
+
+namespace Booma
+{
+	/// <summary>
+	/// PSOBB packet header adapter for the GladNet <see cref="IPacketHeader"/> interface.
+	/// </summary>
+	public sealed class PPSOBBHeaderAdapter : IPacketHeader
+	{
+		/// <inheritdoc />
+		public int PacketSize => Header.PacketSize;
+
+		/// <inheritdoc />
+		public int PayloadSize => Header.PayloadSize;
+
+		/// <summary>
+		/// Adapted header instance.
+		/// </summary>
+		private PSOBBPacketHeader Header { get; }
+
+		public PPSOBBHeaderAdapter([NotNull] PSOBBPacketHeader header)
+		{
+			Header = header ?? throw new ArgumentNullException(nameof(header));
+		}
+	}
+}
