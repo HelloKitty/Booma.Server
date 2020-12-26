49e232a87c1363b769f58ef21da9bfc74051503b
HelloKitty <andrew.blakely@ymail.com>
2020-12-25 22:06:48 -0600

    Added AuthService Program

---
+++ src/Booma.Server.AuthService/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Common.Logging;
+using GladNet;
+
+namespace Booma
+{
+	class Program
+	{
+		public static IPAddress Address { get; } = IPAddress.Parse("127.0.0.1");
+
+		static async Task Main(string[] args)
+		{
+			ILog logger = new ConsoleLogger(LogLevel.All, true);
+			logger.Info($"Starting Login/Auth Service.");
+
+			try
+			{
+				//Auth/Login port for some reason is 12000
+				await new BoomaGameServerApplication(new NetworkAddressInfo(Address, 12000), logger)
+					.BeginListeningAsync();
+			}
+			finally
+			{
+				logger.Warn($"Shutting down Login/Auth Service.");
+			}
+		}
+	}
+}
