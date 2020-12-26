bcaa05a4064a206c64e05ae368bdd7ee0f710be8
HelloKitty <andrew.blakely@ymail.com>
2020-12-26 00:47:03 -0600

    Added demo LoginRequestMessageHandler

---
+++ src/Booma.Server.AuthService/Network/Handlers/Auth/LoginRequestMessageHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Booma.Proxy;
+using Common.Logging;
+using GladNet;
+
+namespace Booma
+{
+	/// <summary>
+	/// <see cref="BaseGameMessageHandler{TMessageType}"/> for the <see cref="SharedLoginRequest93Payload"/>.
+	/// </summary>
+	public sealed class LoginRequestMessageHandler : BaseGameMessageHandler<SharedLoginRequest93Payload>
+	{
+		private ILog Logger { get; }
+
+		public LoginRequestMessageHandler([JetBrains.Annotations.NotNull] ILog logger)
+		{
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+		}
+
+		/// <inheritdoc />
+		public override async Task HandleMessageAsync(SessionMessageContext<PSOBBGamePacketPayloadServer> context, SharedLoginRequest93Payload message, CancellationToken token = default)
+		{
+			if (Logger.IsInfoEnabled)
+				Logger.Info($"Sending Auth Response: Banned for test purposes.");
+
+			await context.MessageService.SendMessageAsync(new SharedLoginResponsePayload(AuthenticationResponseCode.LOGIN_93BB_BANNED), token);
+		}
+	}
+}
