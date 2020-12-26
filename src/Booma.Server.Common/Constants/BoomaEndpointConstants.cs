f9edecc6c5fe02bf206fd9e3bceca37c99f25d04
HelloKitty <andrew.blakely@ymail.com>
2020-12-26 04:56:07 -0600

    Added BoomaEndpointConstants

---
+++ src/Booma.Server.Common/Constants/BoomaEndpointConstants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Booma
+{
+	/// <summary>
+	/// Constants class that contains constant values related to
+	/// endpoints that should be unchanging.
+	/// </summary>
+	public static class BoomaEndpointConstants
+	{
+		/// <summary>
+		/// Represents the default and constant Booma service discovery endpoint.
+		/// </summary>
+		public const string BOOMA_SERVICE_DISCOVERY_ENDPOINT = @"https://localhost:5001/";
+	}
+}
