1e34edb21551a3e9d3f8be15f094c4400108f238
HelloKitty <andrew.blakely@ymail.com>
2020-12-25 21:04:04 -0600

    Added GameMessageHandlerServiceModule

---
+++ src/Booma.Server.AuthService/Autofac/Modules/GameMessageHandlerServiceModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using Booma.Proxy;
+using Common.Logging;
+using GladNet;
+
+namespace Booma
+{
+	/// <summary>
+	/// Game service message handler service module.
+	/// Registers the services and handling for message handlers.
+	/// </summary>
+	public sealed class GameMessageHandlerServiceModule 
+		: ServerMessageHandlerServiceModule<PSOBBGamePacketPayloadClient, PSOBBGamePacketPayloadServer, DefaultBoomaMessageHandler<PSOBBGamePacketPayloadClient, PSOBBGamePacketPayloadServer, GameNetworkOperationCode>>
+	{
+
+	}
+}
