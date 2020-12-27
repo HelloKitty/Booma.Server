778adac15b8b8f2ce1d5c1fcf3b70c4d96136727
HelloKitty <andrew.blakely@ymail.com>
2020-12-27 00:22:02 -0600

    Added ServiceDiscoverableServiceModule

---
+++ src/Booma.Server.LoginService/Autofac/Modules/Services/ServiceDiscoverableServiceModule.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Autofac;
+using Common.Logging;
+using Glader.ASP.ServiceDiscovery;
+
+namespace Booma
+{
+	/// <summary>
+	/// Service module that registers a <see cref="IServiceResolver{TServiceInterfaceType}"/> for the specified
+	/// <typeparamref name="TServiceType"/>. Using <see cref="IServiceDiscoveryService"/> to resolve them.
+	/// </summary>
+	/// <typeparam name="TServiceType"></typeparam>
+	public sealed class ServiceDiscoverableServiceModule<TServiceType> : Module 
+		where TServiceType : class
+	{
+		/// <summary>
+		/// The Booma service type mapping.
+		/// </summary>
+		private BoomaServiceType ServiceType { get; }
+
+		public ServiceDiscoverableServiceModule(BoomaServiceType serviceType)
+		{
+			if (!Enum.IsDefined(typeof(BoomaServiceType), serviceType)) throw new InvalidEnumArgumentException(nameof(serviceType), (int) serviceType, typeof(BoomaServiceType));
+			ServiceType = serviceType;
+		}
+
+		/// <inheritdoc />
+		protected override void Load(ContainerBuilder builder)
+		{
+			base.Load(builder);
+
+			builder.Register<ServiceDiscoveryServiceResolver<TServiceType>>(context =>
+				{
+					return new ServiceDiscoveryServiceResolver<TServiceType>(context.Resolve<IServiceDiscoveryService>(), ServiceType, context.Resolve<ILog>());
+				})
+				.As<IServiceResolver<TServiceType>>()
+				.SingleInstance();
+		}
+	}
+}
