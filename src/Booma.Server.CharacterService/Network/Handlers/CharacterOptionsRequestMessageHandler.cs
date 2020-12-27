6b5c5785588553027b77f92d02c91c49ca1facd9
HelloKitty <andrew.blakely@ymail.com>
2020-12-27 07:17:06 -0600

    Added CharacterOptionsRequestMessageHandler stub

---
+++ src/Booma.Server.CharacterService/Network/Handlers/CharacterOptionsRequestMessageHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Booma.Proxy;
+using Common.Logging;
+using GladNet;
+
+namespace Booma
+{
+	/// <summary>
+	/// Message handler for <see cref="CharacterOptionsRequestPayload"/> which returns a <see cref="CharacterOptionsResponsePayload"/>.
+	/// </summary>
+	public sealed class CharacterOptionsRequestMessageHandler : GameRequestMessageHandler<CharacterOptionsRequestPayload, CharacterOptionsResponsePayload>
+	{
+		private ILog Logger { get; }
+
+		public CharacterOptionsRequestMessageHandler([NotNull] ILog logger)
+		{
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+		}
+
+		/// <inheritdoc />
+		protected override async Task<CharacterOptionsResponsePayload> HandleRequestAsync(SessionMessageContext<PSOBBGamePacketPayloadServer> context, CharacterOptionsRequestPayload message, CancellationToken token = default)
+		{
+			if (Logger.IsDebugEnabled)
+				Logger.Debug($"Client: {context.Details.ConnectionId} sent Options Request.");
+
+			//TODO: This is basically test code, needs to be properly implemented eventually.
+			return new CharacterOptionsResponsePayload(BindingsConfig.CreateDefault(), 1, new AccountTeamInformation(0, new uint[2], 0, 0, String.Empty, 0));
+		}
+	}
+}
