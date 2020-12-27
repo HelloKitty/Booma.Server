1da56b44ed902b2c6eed463f75f83dde96a3c3ac
HelloKitty <andrew.blakely@ymail.com>
2020-12-26 23:15:50 -0600

    Added GetServiceIdentifier to BoomaEndpointConstants

--- src/Booma.Server.Common/Constants/BoomaEndpointConstants.cs
+++ src/Booma.Server.Common/Constants/BoomaEndpointConstants.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace Booma
@@ -14,5 +15,30 @@ namespace Booma
 		/// Represents the default and constant Booma service discovery endpoint.
 		/// </summary>
 		public const string BOOMA_SERVICE_DISCOVERY_ENDPOINT = @"https://localhost:5001/";
+
+		/// <summary>
+		/// Represents the constant string identifier for a Booma service name.
+		/// </summary>
+		public const string BOOMA_AUTHENTICATION_SERVICE_NAME = "AUTHENTICATION";
+
+		/// <summary>
+		/// Gets a constant service name based on the service type.
+		/// </summary>
+		/// <param name="type">The service name.</param>
+		/// <returns></returns>
+		public static string GetServiceIdentifier(BoomaServiceType type)
+		{
+			if (!Enum.IsDefined(typeof(BoomaServiceType), type)) throw new InvalidEnumArgumentException(nameof(type), (int) type, typeof(BoomaServiceType));
+
+			switch (type)
+			{
+				case BoomaServiceType.AuthService:
+					return BOOMA_AUTHENTICATION_SERVICE_NAME;
+				case BoomaServiceType.PatchService:
+				case BoomaServiceType.LoginService:
+				default:
+					throw new ArgumentOutOfRangeException(nameof(type), type, null);
+			}
+		}
 	}
 }
