02590df0b248f97d99c80cfd44cc74911f0c629a
HelloKitty <andrew.blakely@ymail.com>
2020-12-27 03:41:10 -0600

    Made LoginResponseSentEventListener an async handler

--- src/Booma.Server.LoginService/Events/Listeners/Base/LoginResponseSentEventListener.cs
+++ src/Booma.Server.LoginService/Events/Listeners/Base/LoginResponseSentEventListener.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Glader;
 using Glader.Essentials;
 
 namespace Booma
@@ -8,7 +9,7 @@ namespace Booma
 	/// <summary>
 	/// Simplified type signature for <see cref="EventListener{TSubscribableType}"/> for <see cref="ILoginResponseSentEventSubscribable"/>
 	/// </summary>
-	public abstract class LoginResponseSentEventListener : EventListener<ILoginResponseSentEventSubscribable, LoginResponseSentEventArgs>
+	public abstract class LoginResponseSentEventListener : EventListenerAsync<ILoginResponseSentEventSubscribable, LoginResponseSentEventArgs>
 	{
 		protected LoginResponseSentEventListener(ILoginResponseSentEventSubscribable subscriptionService) 
 			: base(subscriptionService)
