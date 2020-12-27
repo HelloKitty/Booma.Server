1d8fd998de63f5c0e997c4b09993bb482df1d344
HelloKitty <andrew.blakely@ymail.com>
2020-12-27 03:01:39 -0600

    Added LoginResponseSentEventListener

---
+++ src/Booma.Server.LoginService/Events/Listeners/Base/LoginResponseSentEventListener.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace Booma
+{
+	/// <summary>
+	/// Simplified type signature for <see cref="EventListener{TSubscribableType}"/> for <see cref="ILoginResponseSentEventSubscribable"/>
+	/// </summary>
+	public abstract class LoginResponseSentEventListener : EventListener<ILoginResponseSentEventSubscribable, LoginResponseSentEventArgs>
+	{
+		protected LoginResponseSentEventListener(ILoginResponseSentEventSubscribable subscriptionService) 
+			: base(subscriptionService)
+		{
+
+		}
+	}
+}
