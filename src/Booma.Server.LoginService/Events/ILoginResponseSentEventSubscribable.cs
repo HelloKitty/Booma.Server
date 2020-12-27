7cdff93be31bedf4d5391cfa42a6f433fc09ba6b
HelloKitty <andrew.blakely@ymail.com>
2020-12-27 02:41:42 -0600

    Added ILoginResponseSentEventSubscribable

---
+++ src/Booma.Server.LoginService/Events/ILoginResponseSentEventSubscribable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using FreecraftCore;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Booma.Proxy;
+using GladNet;
+
+namespace Booma
+{
+	/// <summary>
+	/// Contract for a type that implements a subscription service for events that publish <see cref="LoginResponseSentEventArgs"/>
+	/// through the <see cref="EventHandler{TEventArgs}"/> <see cref="OnLoginResponseSent"/>
+	/// </summary>
+	public interface ILoginResponseSentEventSubscribable
+	{
+		event EventHandler<LoginResponseSentEventArgs> OnLoginResponseSent;
+	}
+
+	/// <summary>
+	/// Event arguments for the <see cref="ILoginResponseSentEventSubscribable"/> interface.
+	/// </summary>
+	public sealed class LoginResponseSentEventArgs : EventArgs, IResponseCodePayload<AuthenticationResponseCode>
+	{
+		/// <inheritdoc />
+		public AuthenticationResponseCode ResponseCode { get; }
+
+		//This is dumb, we should flow a connection identifier or something. Not a freaking message context.
+		/// <summary>
+		/// The context of the login message.
+		/// </summary>
+		public SessionMessageContext<PSOBBGamePacketPayloadServer> MessageContext { get; }
+
+		public LoginResponseSentEventArgs(AuthenticationResponseCode responseCode, [NotNull] SessionMessageContext<PSOBBGamePacketPayloadServer> messageContext)
+		{
+			if (!Enum.IsDefined(typeof(AuthenticationResponseCode), responseCode)) throw new InvalidEnumArgumentException(nameof(responseCode), (int) responseCode, typeof(AuthenticationResponseCode));
+			ResponseCode = responseCode;
+			MessageContext = messageContext ?? throw new ArgumentNullException(nameof(messageContext));
+		}
+	}
+}
