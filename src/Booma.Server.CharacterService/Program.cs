de75bfb9cb49a0d2c6808672c8cb58b3db36589b
HelloKitty <andrew.blakely@ymail.com>
2020-12-27 05:28:26 -0600

    Created Character Service project

---
+++ src/Booma.Server.CharacterService/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Common.Logging;
+using GladNet;
+
+namespace Booma
+{
+	class Program
+	{
+		public static IPAddress Address { get; } = IPAddress.Parse("127.0.0.1");
+
+		static async Task Main(string[] args)
+		{
+			ILog logger = new ConsoleLogger(LogLevel.All, true);
+			logger.Info($"Starting Character Service.");
+
+			try
+			{
+				//Character port for some reason is 12001
+				await new BoomaCharacterServerApplication(new NetworkAddressInfo(Address, 12001), logger)
+					.BeginListeningAsync();
+			}
+			finally
+			{
+				logger.Warn($"Shutting down Character Service.");
+			}
+		}
+	}
+}
--- src/Booma.Server.Game/Modules/LoginServiceModule.cs
+++ src/Booma.Server.Game/Modules/LoginServiceModule.cs
@@ -8,13 +8,13 @@ using Glader.ASP.Authentication;
 
 namespace Booma
 {
+	//This exists for services that don't want any special event to occur
+	//when login responses happen.
 	/// <summary>
 	/// Service module for all the services required to properly handle
 	/// <see cref="SharedLoginRequest93Payload"/> and <see cref="LoginRequestMessageHandler"/>
-	/// <typeparam name="TLoginResponseHandlingStrategyType">Represents the handling strategy for login responses.</typeparam>
 	/// </summary>
-	public sealed class LoginServiceModule<TLoginResponseHandlingStrategyType> : Module
-		where TLoginResponseHandlingStrategyType : LoginResponseSentEventListener
+	public class LoginServiceModule : Module
 	{
 		/// <inheritdoc />
 		protected override void Load(ContainerBuilder builder)
@@ -27,6 +27,21 @@ namespace Booma
 			builder.RegisterType<SuccessfulLogin93ResponseMessageFactory>()
 				.AsImplementedInterfaces()
 				.SingleInstance();
+		}
+	}
+
+	/// <summary>
+	/// Service module for all the services required to properly handle
+	/// <see cref="SharedLoginRequest93Payload"/> and <see cref="LoginRequestMessageHandler"/>
+	/// <typeparam name="TLoginResponseHandlingStrategyType">Represents the handling strategy for login responses.</typeparam>
+	/// </summary>
+	public sealed class LoginServiceModule<TLoginResponseHandlingStrategyType> : LoginServiceModule
+		where TLoginResponseHandlingStrategyType : LoginResponseSentEventListener
+	{
+		/// <inheritdoc />
+		protected override void Load(ContainerBuilder builder)
+		{
+			base.Load(builder);
 
 			//This is the most critical handler for login response.
 			//It should start-off the whole session or redirect it or something.
