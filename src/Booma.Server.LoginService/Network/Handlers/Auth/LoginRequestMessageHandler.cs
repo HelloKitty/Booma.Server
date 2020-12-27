beecfb9488892540721668cb1acebc4ef5345454
HelloKitty <andrew.blakely@ymail.com>
2020-12-27 00:22:26 -0600

    Rewrote Login93 handler to use Auth Service resolver

--- src/Booma.Server.LoginService/Network/Handlers/Auth/LoginRequestMessageHandler.cs
+++ src/Booma.Server.LoginService/Network/Handlers/Auth/LoginRequestMessageHandler.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Booma.Proxy;
 using Common.Logging;
+using Glader.ASP.Authentication;
+using GladMMO;
 using GladNet;
+using Refit;
 
 namespace Booma
 {
@@ -14,21 +18,83 @@ namespace Booma
 	/// </summary>
 	public sealed class LoginRequestMessageHandler : GameRequestMessageHandler<SharedLoginRequest93Payload, SharedLoginResponsePayload>
 	{
+		/// <summary>
+		/// Logging service.
+		/// </summary>
 		private ILog Logger { get; }
 
-		public LoginRequestMessageHandler([NotNull] ILog logger)
+		/// <summary>
+		/// The service resolver for an <see cref="IAuthenticationService"/>.
+		/// </summary>
+		private IServiceResolver<IAuthenticationService> AuthenticationServiceResolver { get; }
+
+		public LoginRequestMessageHandler([NotNull] ILog logger,
+			[NotNull] IServiceResolver<IAuthenticationService> authenticationServiceResolver)
 		{
 			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+			AuthenticationServiceResolver = authenticationServiceResolver ?? throw new ArgumentNullException(nameof(authenticationServiceResolver));
 		}
 
 		/// <inheritdoc />
 		protected override async Task<SharedLoginResponsePayload> HandleRequestAsync(SessionMessageContext<PSOBBGamePacketPayloadServer> context, 
 			SharedLoginRequest93Payload message, CancellationToken token = default)
 		{
-			if(Logger.IsInfoEnabled)
-				Logger.Info($"Sending Auth Response: Banned for test purposes.");
+			if(Logger.IsDebugEnabled)
+				Logger.Debug($"Attempting to Authenticate: {message.UserName}");
 
-			return new SharedLoginResponsePayload(AuthenticationResponseCode.LOGIN_93BB_BANNED);
+			try
+			{
+				return await TryAuthenticateUserAsync(message, token);
+			}
+			catch (Exception e)
+			{
+				if(Logger.IsErrorEnabled)
+					Logger.Error($"Authentication exception. Reason: {e}");
+
+				return new SharedLoginResponsePayload(AuthenticationResponseCode.LOGIN_93BB_UNKNOWN_ERROR);
+			}
+		}
+
+		private async Task<SharedLoginResponsePayload> TryAuthenticateUserAsync([NotNull] SharedLoginRequest93Payload message, CancellationToken token)
+		{
+			if (message == null) throw new ArgumentNullException(nameof(message));
+
+			//Resolve the service, or try to.
+			ServiceResolveResult<IAuthenticationService> authResolutionResult =
+				await AuthenticationServiceResolver.Create(token);
+
+			//If we have no auth service then we cannot auth the user
+			//there isn't really a good response code for this so unknown error will have to do.
+			if (!authResolutionResult.isAvailable)
+				return new SharedLoginResponsePayload(AuthenticationResponseCode.LOGIN_93BB_UNKNOWN_ERROR);
+
+			IAuthenticationService authService = authResolutionResult.Instance;
+
+			try
+			{
+				JWTModel authResult = await authService.AuthenticateAsync(new AuthenticationRequest(message.UserName, message.Password));
+
+				//TODO: Send more accurate error response.
+				//If token is valid then auth was successful, otherwise something is wrong with credentials.
+				if (authResult.isTokenValid)
+					return new SharedLoginResponsePayload(AuthenticationResponseCode.LOGIN_93BB_OK);
+				else
+					return new SharedLoginResponsePayload(AuthenticationResponseCode.LOGIN_93BB_BAD_USER_PWD);
+			}
+			catch (ApiException e)
+			{
+				//This is an expected exception type due to the specification sending 400 BAD REQUEST on failure.
+				if (e.StatusCode != HttpStatusCode.BadRequest)
+					throw;
+
+				JWTModel authResult = await e.GetContentAsAsync<JWTModel>();
+
+				if (Logger.IsInfoEnabled)
+					Logger.Info($"Account: {message.UserName} failed Auth. Reason: {authResult.Error} Message: {authResult.ErrorDescription}");
+
+				//TODO: Handle error cases
+				return new SharedLoginResponsePayload(AuthenticationResponseCode.LOGIN_93BB_BAD_USER_PWD);
+			}
 		}
 	}
 }
