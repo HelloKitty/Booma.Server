615610232de284672d29335a5b1efd015422c3b2
HelloKitty <andrew.blakely@ymail.com>
2020-12-26 23:15:12 -0600

    Added BoomaServiceType enum

---
+++ src/Booma.Server.Common/Network/BoomaServiceType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Booma
+{
+	/// <summary>
+	/// Enumerations of all Booma service types.
+	/// </summary>
+	public enum BoomaServiceType
+	{
+		/// <summary>
+		/// The patch service/server.
+		/// </summary>
+		PatchService = 0,
+
+		/// <summary>
+		/// The login/entry point service/server.
+		/// </summary>
+		LoginService = 1,
+
+		/// <summary>
+		/// The Authentication service.
+		/// </summary>
+		AuthService = 2,
+	}
+}
