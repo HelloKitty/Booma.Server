6576e82dc3afb5b16baeef49e921a61d0c129201
HelloKitty <andrew.blakely@ymail.com>
2020-12-27 23:31:26 -0600

    Implement basic CharacterChecksumRequestPayload handling

---
+++ src/Booma.Server.CharacterService/Network/Handlers/CharacterChecksumRequestMessageHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Booma.Proxy;
+using GladNet;
+
+namespace Booma
+{
+	/// <summary>
+	/// Handler for <see cref="CharacterChecksumRequestPayload"/> that sends <see cref="CharacterChecksumResponsePayload"/> as a response.
+	/// </summary>
+	public sealed class CharacterChecksumRequestMessageHandler : GameRequestMessageHandler<CharacterChecksumRequestPayload, CharacterChecksumResponsePayload>
+	{
+		protected override async Task<CharacterChecksumResponsePayload> HandleRequestAsync(SessionMessageContext<PSOBBGamePacketPayloadServer> context, CharacterChecksumRequestPayload message, CancellationToken token = default)
+		{
+			//TODO: Actually validate this checksum.
+			return new CharacterChecksumResponsePayload(LoginChecksumResult.Success);
+		}
+	}
+}
