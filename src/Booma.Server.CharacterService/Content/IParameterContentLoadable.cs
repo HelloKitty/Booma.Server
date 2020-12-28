bedba45b84c768613de53d927acc71555edbd1da
HelloKitty <andrew.blakely@ymail.com>
2020-12-28 05:08:15 -0600

    Added IParameterContentLoadable

---
+++ src/Booma.Server.CharacterService/Content/IParameterContentLoadable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Booma.Proxy;
+
+namespace Booma
+{
+	/// <summary>
+	/// Contract for a strategy for loading <see cref="DataParameterFileHeader"/>s and the respective binary content.
+	/// </summary>
+	public interface IParameterContentLoadable
+	{
+		/// <summary>
+		/// Loads the <see cref="DataParameterFileHeader"/>s.
+		/// </summary>
+		/// <returns>The parameter heads.</returns>
+		Task<DataParameterFileHeader[]> LoadHeadersAsync();
+
+		/// <summary>
+		/// Loads a data parameter buffer based on the provided <see cref="header"/>.
+		/// </summary>
+		/// <param name="header">The header for the parameter content/</param>
+		/// <returns>The binary buffer representing the parameters.</returns>
+		Task<byte[]> GetParameterDataAsync(DataParameterFileHeader header);
+
+		/// <summary>
+		/// Loads a binary buffer linked to the specified <see cref="chunkId"/>.
+		/// </summary>
+		/// <param name="chunkId">The chunk id.</param>
+		/// <returns>The buffer linked to the id.</returns>
+		Task<byte[]> GetParameterDataChunkAsync(int chunkId);
+	}
+}
