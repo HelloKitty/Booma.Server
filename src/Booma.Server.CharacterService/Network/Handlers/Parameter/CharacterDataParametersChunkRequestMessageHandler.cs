3fdf45cce2e77dce72d6378134b0652683e2f281
HelloKitty <andrew.blakely@ymail.com>
2020-12-28 05:09:47 -0600

    Implemented parameter chunk message handlers

---
+++ src/Booma.Server.CharacterService/Network/Handlers/Parameter/CharacterDataParametersChunkRequestMessageHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Booma.Proxy;
+using GladNet;
+
+namespace Booma
+{
+	/// <summary>
+	/// Message handler for <see cref="CharacterDataParametersChunkRequestPayload"/>. Sends <see cref="CharacterDataParametersChunkResponsePayload"/> as a response.
+	/// This basically streams the binary content for parameter data to the client based on chunk id.
+	/// </summary>
+	public sealed class CharacterDataParametersChunkRequestMessageHandler : GameRequestMessageHandler<CharacterDataParametersChunkRequestPayload, CharacterDataParametersChunkResponsePayload>
+	{
+		private IParameterContentLoadable ParameterLoader { get; }
+
+		public CharacterDataParametersChunkRequestMessageHandler([NotNull] IParameterContentLoadable parameterLoader) 
+			: base(true)
+		{
+			ParameterLoader = parameterLoader ?? throw new ArgumentNullException(nameof(parameterLoader));
+		}
+
+		protected override async Task<CharacterDataParametersChunkResponsePayload> HandleRequestAsync(SessionMessageContext<PSOBBGamePacketPayloadServer> context, CharacterDataParametersChunkRequestPayload message, CancellationToken token = default)
+		{
+			//See reference: https://github.com/Sylverant/login_server/blob/12631a7035c68d6a8a99e3d0f524e7892554e6e7/src/bbcharacter.c#L943
+			byte[] bytes = await ParameterLoader.GetParameterDataChunkAsync((int) message.ChunkNumber);
+
+			return new CharacterDataParametersChunkResponsePayload(message.ChunkNumber, bytes);
+		}
+	}
+}
---
+++ src/Booma.Server.CharacterService/Network/Handlers/Parameter/CharacterDataParametersHeaderRequestMessageHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Booma.Proxy;
+using GladNet;
+
+namespace Booma
+{
+	/// <summary>
+	/// Handler for <see cref="CharacterDataParametersHeaderRequestPayload"/>. Clients send this message when they want to load
+	/// the serverside parameter content.
+	/// </summary>
+	public sealed class CharacterDataParametersHeaderRequestMessageHandler : GameRequestMessageHandler<CharacterDataParametersHeaderRequestPayload, CharacterDataParametersHeaderResponsePayload>
+	{
+		/// <summary>
+		/// Parameter content loading service.
+		/// </summary>
+		private IParameterContentLoadable ParameterLoader { get; }
+
+		public CharacterDataParametersHeaderRequestMessageHandler([NotNull] IParameterContentLoadable parameterLoader) 
+			: base(true)
+		{
+			ParameterLoader = parameterLoader ?? throw new ArgumentNullException(nameof(parameterLoader));
+		}
+
+		protected override async Task<CharacterDataParametersHeaderResponsePayload> HandleRequestAsync(SessionMessageContext<PSOBBGamePacketPayloadServer> context, CharacterDataParametersHeaderRequestPayload message, CancellationToken token = default)
+		{
+			//For the concept of what this is check documentation on parameter file heads.
+			//A reference for concept: https://github.com/Sylverant/login_server/blob/12631a7035c68d6a8a99e3d0f524e7892554e6e7/src/bbcharacter.c#L943
+			DataParameterFileHeader[] headers = await ParameterLoader.LoadHeadersAsync();
+			return new CharacterDataParametersHeaderResponsePayload(headers);
+		}
+	}
+}
