e8693f394134e7d023b3de492b9708cf189d83aa
HelloKitty <andrew.blakely@ymail.com>
2020-12-27 23:34:15 -0600

    Added GameCommandMessageHandler

---
+++ src/Booma.Server.Game/Network/Handlers/Base/GameCommandMessageHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Booma.Proxy;
+using GladNet;
+
+namespace Booma
+{
+	/// <summary>
+	/// Special <see cref="GameMessageHandler{TMessageType}"/> that implements command semantics (empty payload).
+	/// </summary>
+	/// <typeparam name="TMessageRequestType"></typeparam>
+	public abstract class GameCommandMessageHandler<TMessageRequestType> : GameMessageHandler<TMessageRequestType> 
+		where TMessageRequestType : PSOBBGamePacketPayloadClient
+	{
+		/// <inheritdoc />
+		public sealed override Task HandleMessageAsync(SessionMessageContext<PSOBBGamePacketPayloadServer> context, TMessageRequestType message, CancellationToken token = default)
+		{
+			return HandleCommandAsync(context, token);
+		}
+
+		/// <summary>
+		/// Similar to <see cref="HandleMessageAsync"/> but doesn't provide the message itself as it is an empty message.
+		/// </summary>
+		/// <param name="context">Message context.</param>
+		/// <param name="token">Cancel token.</param>
+		/// <returns></returns>
+		protected abstract Task HandleCommandAsync(SessionMessageContext<PSOBBGamePacketPayloadServer> context, CancellationToken token = default);
+	}
+}
