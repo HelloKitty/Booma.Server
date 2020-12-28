3ecd7f1eb6107c3d9dc25172839c1e4e6abf184b
HelloKitty <andrew.blakely@ymail.com>
2020-12-27 22:51:35 -0600

    Added initial handling CharacterOptionsUpdateRequestMessageHandler

---
+++ src/Booma.Server.CharacterService/Network/Handlers/CharacterOptionsUpdateRequestMessageHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Booma.Proxy;
+using Common.Logging;
+using GladNet;
+
+namespace Booma
+{
+	//TODO: Need persistence.
+	//Client can send this if it disagrees with the options data sent down or wants to update their options.
+	/// <summary>
+	/// Message handler for <see cref="CharacterOptionsRequestPayload"/> which returns a <see cref="CharacterOptionsResponsePayload"/>.
+	/// </summary>
+	public sealed class CharacterOptionsUpdateRequestMessageHandler : GameRequestMessageHandler<CharacterOptionsUpdateRequestPayload, CharacterOptionsResponsePayload>
+	{
+		protected override async Task<CharacterOptionsResponsePayload> HandleRequestAsync(SessionMessageContext<PSOBBGamePacketPayloadServer> context, CharacterOptionsUpdateRequestPayload message, CancellationToken token = default)
+		{
+			//TODO: We should save and persist these options.
+			//Just ehco it back as an options response for now.
+			return new CharacterOptionsResponsePayload(message.Bindings, message.GuildCard, message.TeamInfo);
+		}
+	}
+}
