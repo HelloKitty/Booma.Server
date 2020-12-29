fe56f918f82cb6f207e97a052adb184ce04d0ad9
HelloKitty <andrew.blakely@ymail.com>
2020-12-29 04:00:03 -0600

    Added RPGCharacter library implementation

--- src/Booma.Server.CharacterDataService/Startup.cs
+++ src/Booma.Server.CharacterDataService/Startup.cs
@@ -9,7 +9,13 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using Glader.ASP.RPGCharacter;
+using Glader.Essentials;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Booma.Server.CharacterDataService
 {
@@ -25,7 +31,40 @@ namespace Booma.Server.CharacterDataService
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddControllers();
+			services.AddControllers()
+				.RegisterCharacterDataController()
+				.AddNewtonsoftJson();
+
+			services.RegisterCharacterDatabase(builder =>
+			{
+				builder.UseMySql("server=127.0.0.1;port=3306;Database=booma.game;Uid=root;Pwd=test;", optionsBuilder =>
+				{
+					optionsBuilder.MigrationsAssembly(GetType().Assembly.FullName);
+				});
+			});
+
+			services.RegisterGladerASP();
+
+			//TODO: Support real certs. This is a complete DEV ONLY HACK!
+			services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+				.AddJwtBearer(options =>
+				{
+					//TODO: This is demo test signing code from: https://github.com/openiddict/openiddict-core/blob/7e1c9dd1307f8127288682459b0b1d82ea804e4f/src/OpenIddict.Server/OpenIddictServerBuilder.cs#L647
+					using var store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+					store.Open(OpenFlags.ReadWrite);
+
+					var subject = new X500DistinguishedName("CN=OpenIddict Server Signing Certificate");
+					var certificate = store.Certificates.Find(X509FindType.FindBySubjectDistinguishedName, subject.Name, validOnly: false)
+						.OfType<X509Certificate2>()
+						.SingleOrDefault();
+
+					options.TokenValidationParameters
+						.IssuerSigningKey = new X509SecurityKey(certificate);
+
+					//TODO: This audience stuff is ALL WRONG.
+					options.Audience = "auth-server";
+					options.TokenValidationParameters.ValidIssuer = "https://localhost:5003/";
+				});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
