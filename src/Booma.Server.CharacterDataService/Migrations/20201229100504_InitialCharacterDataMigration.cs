cfd1b438d3ff5c07a6d438642b9bca990986841c
HelloKitty <andrew.blakely@ymail.com>
2020-12-29 04:05:42 -0600

    Ran RPG Character DB Migrations

---
+++ src/Booma.Server.CharacterDataService/Migrations/20201229100504_InitialCharacterDataMigration.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Booma.Server.CharacterDataService.Migrations
+{
+    public partial class InitialCharacterDataMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "character_progress",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Experience = table.Column<int>(nullable: false),
+                    Level = table.Column<int>(nullable: false),
+                    PlayTime = table.Column<TimeSpan>(nullable: false, defaultValue: new TimeSpan(0, 0, 0, 0, 0)),
+                    LastModifiedDate = table.Column<DateTime>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.ComputedColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_character_progress", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "character",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: false),
+                    CreationDate = table.Column<DateTime>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    LastModifiedDate = table.Column<DateTime>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.ComputedColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_character", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_character_character_progress_Id",
+                        column: x => x.Id,
+                        principalTable: "character_progress",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "character_ownership",
+                columns: table => new
+                {
+                    OwnershipId = table.Column<int>(nullable: false),
+                    CharacterId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_character_ownership", x => new { x.OwnershipId, x.CharacterId });
+                    table.ForeignKey(
+                        name: "FK_character_ownership_character_CharacterId",
+                        column: x => x.CharacterId,
+                        principalTable: "character",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_character_ownership_CharacterId",
+                table: "character_ownership",
+                column: "CharacterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_character_ownership_OwnershipId",
+                table: "character_ownership",
+                column: "OwnershipId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "character_ownership");
+
+            migrationBuilder.DropTable(
+                name: "character");
+
+            migrationBuilder.DropTable(
+                name: "character_progress");
+        }
+    }
+}
