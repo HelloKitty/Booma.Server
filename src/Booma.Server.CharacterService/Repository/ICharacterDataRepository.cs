d99bbd8337c26991be5ef50f09cccd16fd9f962e
HelloKitty <andrew.blakely@ymail.com>
2020-12-30 03:31:12 -0600

    Created ICharacterDataRepository

---
+++ src/Booma.Server.CharacterService/Repository/ICharacterDataRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Booma.Proxy;
+
+namespace Booma
+{
+	/// <summary>
+	/// Contract for type that provide a data access interface to character data.
+	/// </summary>
+	public interface ICharacterDataRepository
+	{
+		/// <summary>
+		/// Indicates if a character exists mapped to the specified <see cref="slot"/>
+		/// </summary>
+		/// <param name="slot">The character slot.</param>
+		/// <param name="token"></param>
+		/// <returns>True if the character exists.</returns>
+		Task<bool> ContainsAsync(int slot, CancellationToken token = default);
+
+		/// <summary>
+		/// Retrieves the character data mapped to the specified <see cref="slot"/>.
+		/// </summary>
+		/// <param name="slot">The character slot.</param>
+		/// <param name="token"></param>
+		/// <returns>True if the character exists.</returns>
+		Task<PlayerCharacterDataModel> RetrieveAsync(int slot, CancellationToken token = default);
+	}
+}
