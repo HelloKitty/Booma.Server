23f9a410753bf1a6a33c193eb9fee10b5a913c44
HelloKitty <andrew.blakely@ymail.com>
2020-12-30 02:44:16 -0600

    Added CharacterDataServiceModule

---
+++ src/Booma.Server.CharacterService/Modules/CharacterDataServiceModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using Glader.ASP.RPGCharacter;
+
+namespace Booma
+{
+	/// <summary>
+	/// Service module that registers <see cref="ICharacterDataQueryService"/>
+	/// and <see cref="ICharacterCreationService"/> via the <see cref="IServiceResolver{TServiceInterfaceType}"/>.
+	/// </summary>
+	public sealed class CharacterDataServiceModule : Module
+	{
+		/// <inheritdoc />
+		protected override void Load(ContainerBuilder builder)
+		{
+			base.Load(builder);
+
+			builder.RegisterModule(new ServiceDiscoverableServiceModule<ICharacterDataQueryService>(BoomaServiceType.CharacterDataService));
+			builder.RegisterModule(new ServiceDiscoverableServiceModule<ICharacterCreationService>(BoomaServiceType.CharacterDataService));
+		}
+	}
+}
