ea285b8715ad1e9fc7e702c50cbf7a76e7e7ff4c
HelloKitty <andrew.blakely@ymail.com>
2020-12-31 03:36:20 -0600

    Added LambdaNetworkButtonScript

---
+++ src/Booma.Server.Game/UI/Menu/Button/LambdaNetworkButtonScript.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Booma
+{
+	public sealed class LambdaNetworkButtonScript : NetworkButtonScript
+	{
+		private Func<Task> InternalLambda { get; }
+
+		public LambdaNetworkButtonScript(Func<Task> internalLambda)
+		{
+			InternalLambda = internalLambda ?? throw new ArgumentNullException(nameof(internalLambda));
+		}
+
+		public override async Task OnSelectionAsync(CancellationToken token = default)
+		{
+			await InternalLambda.Invoke();
+		}
+	}
+}
