b2ae6a5238af930e314e06c6659887b9857f7e61
HelloKitty <andrew.blakely@ymail.com>
2021-01-07 02:07:39 -0600

    Added LobbyEntry data repository

---
+++ src/Booma.Server.ChannelService/Services/Data/InMemoryLobbyEntryRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Booma
+{
+	/// <summary>
+	/// In-memory implementation of <see cref="ILobbyEntryRepository"/>
+	/// </summary>
+	public sealed class InMemoryLobbyEntryRepository : ILobbyEntryRepository
+	{
+		private ConcurrentDictionary<int, LobbyEntry> InternalStore { get; } = new ConcurrentDictionary<int, LobbyEntry>();
+
+		/// <inheritdoc />
+		public Task<bool> ContainsAsync(int key, CancellationToken token = default)
+		{
+			return Task.FromResult(InternalStore.ContainsKey(key));
+		}
+
+		/// <inheritdoc />
+		public Task<bool> TryCreateAsync(LobbyEntry model, CancellationToken token = default)
+		{
+			return Task.FromResult(InternalStore.TryAdd(model.LobbyId, model));
+		}
+
+		/// <inheritdoc />
+		public Task<LobbyEntry> RetrieveAsync(int key, CancellationToken token = default, bool includeNavigationProperties = false)
+		{
+			return Task.FromResult(InternalStore[key]);
+		}
+
+		/// <inheritdoc />
+		public Task<bool> TryDeleteAsync(int key, CancellationToken token = default)
+		{
+			return Task.FromResult(InternalStore.Remove(key, out var _));
+		}
+
+		/// <inheritdoc />
+		public Task UpdateAsync(int key, LobbyEntry model, CancellationToken token = default)
+		{
+			return Task.FromResult(InternalStore[key] = model);
+		}
+
+		/// <inheritdoc />
+		public Task<LobbyEntry[]> RetrieveAllAsync(CancellationToken token = new CancellationToken())
+		{
+			//TODO: This copy is kinda shitty.
+			//We should cache it!
+			return Task.FromResult(InternalStore.Values.ToArray());
+		}
+	}
+}
---
+++ src/Booma.Server.ChannelService/Services/Data/LobbyEntry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Booma
+{
+	/// <summary>
+	/// Simple data model for a lobby entry.
+	/// </summary>
+	public sealed class LobbyEntry
+	{
+		/// <summary>
+		/// The id of the lobby entry.
+		/// </summary>
+		public int LobbyId { get; }
+
+		//TODO: Include player count.
+
+		public LobbyEntry(int lobbyId)
+		{
+			if (lobbyId < 0) throw new ArgumentOutOfRangeException(nameof(lobbyId));
+
+			LobbyId = lobbyId;
+		}
+	}
+}
