3b78e6a402b29e81e98777dcbd8546d540e64819
HelloKitty <andrew.blakely@ymail.com>
2021-01-07 10:44:59 -0600

    Created CharacterLobbySlot repository

---
+++ src/Booma.Server.ChannelService/Services/Data/Lobby/CharacterLobbySlot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Akka.Actor;
+
+namespace Booma
+{
+	/// <summary>
+	/// Data model that represents a character lobby slot.
+	/// </summary>
+	public sealed class CharacterLobbySlot
+	{
+		public InitialCharacterDataSnapshot CharacterData { get; }
+
+		public int Slot { get; }
+
+		public IActorRef Actor { get; }
+
+		public CharacterLobbySlot(InitialCharacterDataSnapshot characterData, int slot, IActorRef actor)
+		{
+			if (slot < 0) throw new ArgumentOutOfRangeException(nameof(slot));
+			CharacterData = characterData ?? throw new ArgumentNullException(nameof(characterData));
+			Slot = slot;
+			Actor = actor ?? throw new ArgumentNullException(nameof(actor));
+		}
+	}
+}
---
+++ src/Booma.Server.ChannelService/Services/Data/Lobby/ICharacterLobbySlotRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Glader.Essentials;
+
+namespace Booma
+{
+	public interface ICharacterLobbySlotRepository : IGenericRepositoryCrudable<int, CharacterLobbySlot>, IEntireTableQueryable<CharacterLobbySlot>
+	{
+		/// <summary>
+		/// Indicates if a slot is available in the lobby.
+		/// </summary>
+		/// <param name="token">Cancel token.</param>
+		/// <returns>True if a slot is available.</returns>
+		Task<bool> HasAvailableSlotAsync(CancellationToken token = default);
+
+		/// <summary>
+		/// Indicates the first available slot value for the character lobby slot.
+		/// </summary>
+		/// <param name="token">Cancel token.</param>
+		/// <returns>Available slot.</returns>
+		Task<int> FirstAvailableSlotAsync(CancellationToken token = default);
+	}
+}
