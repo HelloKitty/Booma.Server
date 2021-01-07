300e2177f8253c3a7a705b67659bb878467f2260
HelloKitty <andrew.blakely@ymail.com>
2021-01-07 10:56:36 -0600

    Added LobbyCharacterActor

---
+++ src/Booma.Server.ChannelService/Engine/Akka/Actors/LobbyCharacterActor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Common.Logging;
+using Glader.Essentials;
+using MEAKKA;
+
+namespace Booma
+{
+	public sealed class LobbyCharacterActor : BaseEntityActor<EmptyFactoryContext>
+	{
+		//We have to use specific type because it contains actor type information.
+		public LobbyCharacterActor(ILog logger, DefaultActorMessageHandlerService<LobbyCharacterActor> messageHandlerService)
+			: base(logger, messageHandlerService)
+		{
+
+		}
+
+		protected override void OnInitialized(EntityActorInitializationSuccessMessage successMessage)
+		{
+			if(Logger.IsInfoEnabled)
+				Logger.Info($"Character Actor Initialized. Path: {Self.Path}");
+		}
+	}
+}
