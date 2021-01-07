e1a78e418e698453a27945f3366cae3ee8763707
HelloKitty <andrew.blakely@ymail.com>
2021-01-07 08:16:39 -0600

    Added TryCreateCharacterActorMessage

---
+++ src/Booma.Server.ChannelService/Engine/Akka/Messages/Shared/TryCreateCharacterActorMessage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MEAKKA;
+
+namespace Booma
+{
+	/// <summary>
+	/// Tells an actor that a character would like to join the System.
+	/// </summary>
+	public sealed class TryCreateCharacterActorMessage : EntityActorMessage
+	{
+		/// <summary>
+		/// The input character data to use for creating
+		/// the character actor.
+		/// </summary>
+		public InitialCharacterDataSnapshot CharacterData { get; }
+
+		public TryCreateCharacterActorMessage(InitialCharacterDataSnapshot characterData)
+		{
+			CharacterData = characterData ?? throw new ArgumentNullException(nameof(characterData));
+		}
+	}
+}
