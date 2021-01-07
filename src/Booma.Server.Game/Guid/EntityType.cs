e97de5133e5f873296716aa32d30d4dff5e03050
HelloKitty <andrew.blakely@ymail.com>
2021-01-07 08:08:33 -0600

    Added NetworkEntityGuid

---
+++ src/Booma.Server.Game/Guid/EntityType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Booma
+{
+	//We use the default values from WoW for now.
+	/// <summary>
+	/// Enumeration of entity types.
+	/// </summary>
+	public enum EntityType : byte
+	{
+		Unknown = 0,
+		Item = 1,
+		Container = 2,
+		Creature = 3,
+		Player = 4,
+		GameObject = 5,
+		DynamicObject = 6,
+		Corpse = 7
+	}
+}
---
+++ src/Booma.Server.Game/Guid/NetworkEntityGuid.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace Booma
+{
+	public sealed class NetworkEntityGuid : ObjectGuid<EntityType>
+	{
+		public NetworkEntityGuid(EntityType type, int id)
+			: this(type, id, 0)
+		{
+
+		}
+
+		public NetworkEntityGuid(EntityType type, int id, int entry)
+		{
+			SetObjectType(type);
+			SetIdentifier(id);
+			SetEntry(entry);
+
+			//TODO: Support shards
+			SetShard(0);
+		}
+	}
+}
