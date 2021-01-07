304206788b45c4696b578a0069c930f1220bbcec
HelloKitty <andrew.blakely@ymail.com>
2021-01-07 09:57:05 -0600

    Added new IActorRequestMessage to TryCreateCharacterRequestMessage

--- src/Booma.Server.ChannelService/Engine/Akka/Messages/Shared/TryCreateCharacterRequestMessage.cs
+++ src/Booma.Server.ChannelService/Engine/Akka/Messages/Shared/TryCreateCharacterRequestMessage.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Glader.Essentials;
 using MEAKKA;
 
 namespace Booma
 {
 	/// <summary>
 	/// Tells an actor that a character would like to join the System.
+	/// Should respond with <see cref="ResponseModel"/> <see cref="CharacterActorCreationResponseCode"/>
+	/// and <see cref="string"/> of the Actor's path.
 	/// </summary>
-	public sealed class TryCreateCharacterRequestMessage : EntityActorMessage
+	public sealed class TryCreateCharacterRequestMessage : EntityActorMessage, IActorRequestMessage<ResponseModel<string, CharacterActorCreationResponseCode>>
 	{
 		/// <summary>
 		/// The input character data to use for creating
