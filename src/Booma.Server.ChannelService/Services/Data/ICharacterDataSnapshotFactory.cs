4d3943a723fb8816df2512a131f65f26cff964f4
HelloKitty <andrew.blakely@ymail.com>
2021-01-07 07:11:00 -0600

    Added InitialCharacterDataSnapshot and Factory

---
+++ src/Booma.Server.ChannelService/Services/Data/ICharacterDataSnapshotFactory.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Glader.ASP.RPGCharacter;
+using Glader.Essentials;
+
+namespace Booma
+{
+	public sealed class CharacterDataEventPayloadCreationContext
+	{
+		/// <summary>
+		/// Indicates the requested slot/index of character to create the
+		/// data event payload fro.
+		/// </summary>
+		public int Slot { get; }
+
+		public CharacterDataEventPayloadCreationContext(int slot)
+		{
+			if (slot < 0) throw new ArgumentOutOfRangeException(nameof(slot));
+
+			Slot = slot;
+		}
+	}
+
+	public interface ICharacterDataSnapshotFactory : IAsyncFactoryCreatable<InitialCharacterDataSnapshot, CharacterDataEventPayloadCreationContext>
+	{
+
+	}
+
+	public sealed class CharacterDataSnapshotFactory : ICharacterDataSnapshotFactory
+	{
+		private IServiceResolver<ICharacterDataQueryService> DataQueryService { get; }
+
+		private ICharacterOptionsConfigurationFactory OptionsFactory { get; }
+
+		public CharacterDataSnapshotFactory(IServiceResolver<ICharacterDataQueryService> dataQueryService, 
+			ICharacterOptionsConfigurationFactory optionsFactory)
+		{
+			DataQueryService = dataQueryService ?? throw new ArgumentNullException(nameof(dataQueryService));
+			OptionsFactory = optionsFactory ?? throw new ArgumentNullException(nameof(optionsFactory));
+		}
+
+		public async Task<InitialCharacterDataSnapshot> Create(CharacterDataEventPayloadCreationContext context)
+		{
+			var serviceQueryResult = await DataQueryService.Create(CancellationToken.None);
+
+			if (!serviceQueryResult.isAvailable)
+				throw new InvalidOperationException($"Failed to aquire service Type: {nameof(ICharacterDataQueryService)}");
+
+			RPGCharacterData[] characterDatas = await serviceQueryResult
+				.Instance
+				.RetrieveCharactersDataAsync();
+
+			//If it don't exist, we throw. Not much else we can do
+			RPGCharacterData data = characterDatas[context.Slot];
+
+			CharacterOptionsConfiguration configuration = await OptionsFactory.Create(CancellationToken.None);
+
+			//new InitializeCharacterDataEventPayload(new CharacterInventoryData(0, 0, 0, 1, Enumerable.Repeat(new InventoryItem(), 30).ToArray()), CreateDefaultCharacterData(), 0, new CharacterBankData(0, Enumerable.Repeat(new BankItem(), 200).ToArray()), new GuildCardEntry(1, "Glader", String.Empty, String.Empty, 1, SectionId.Viridia, CharacterClass.HUmar), 0, configuration)
+			return new InitialCharacterDataSnapshot(CreateEmptyInventory(), CreateEmptyBank(), CreateEmptyStats(), new CharacterProgress((uint)data.Progress.Experience, (uint)data.Progress.Level),
+				new CharacterSpecialCustomInfo(0, CharacterModelType.Regular, 0), new CharacterVersionData(0, 0, 0), new CharacterCustomizationInfo(0, 0, 0, 0, 0, new Vector3<ushort>(0, 0, 0), new Vector2<float>(0, 0)),
+				new GuildCardEntry(1, data.Entry.Name, String.Empty, String.Empty, 1, SectionId.Viridia, CharacterClass.HUmar), configuration);
+		}
+
+		private static CharacterBankData CreateEmptyBank()
+		{
+			return new CharacterBankData(0, Enumerable.Repeat(new BankItem(), 200).ToArray());
+		}
+
+		private CharacterInventoryData CreateEmptyInventory()
+		{
+			return new CharacterInventoryData(0, 0, 0, 1, Enumerable.Repeat(new InventoryItem(), 30).ToArray());
+		}
+
+		//TODO: use object mapper.
+		private LobbyCharacterData Convert(RPGCharacterData character)
+		{
+			if(character == null) throw new ArgumentNullException(nameof(character));
+
+			return new LobbyCharacterData(CreateEmptyStats(), 0, 0, new CharacterProgress((uint)character.Progress.Experience, (uint)character.Progress.Level), 0, String.Empty, 0, new CharacterSpecialCustomInfo(0, CharacterModelType.Regular, 0), SectionId.Viridia, CharacterClass.HUmar,
+				new CharacterVersionData(0, 0, 0), new CharacterCustomizationInfo(0, 0, 0, 0, 0, new Vector3<ushort>(0, 0, 0), new Vector2<float>(0, 0)), character.Entry.Name);
+		}
+
+		private static CharacterStats CreateEmptyStats()
+		{
+			return new CharacterStats(Enumerable.Repeat((ushort)1, 7).ToArray());
+		}
+	}
+}
---
+++ src/Booma.Server.ChannelService/Services/Data/InitialCharacterDataSnapshot.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Booma
+{
+	/// <summary>
+	/// Absolute ridiculous data snapshop of PSOBB character data.
+	/// </summary>
+	public sealed class InitialCharacterDataSnapshot
+	{
+		public CharacterInventoryData Inventory { get; }
+
+		public CharacterBankData Bank { get; }
+
+		public CharacterStats Stats { get; }
+
+		public CharacterProgress Progress { get; }
+
+		public CharacterSpecialCustomInfo SpecialCustom { get; }
+
+		public CharacterVersionData Version { get; }
+
+		public CharacterCustomizationInfo Customization { get; }
+
+		public GuildCardEntry GuildCard { get; }
+
+		public CharacterOptionsConfiguration Options { get; }
+
+		public string Name => GuildCard.Name;
+
+		public InitialCharacterDataSnapshot(CharacterInventoryData inventory, CharacterBankData bank, CharacterStats stats, CharacterProgress progress, CharacterSpecialCustomInfo specialCustom, CharacterVersionData version, CharacterCustomizationInfo customization, GuildCardEntry guildCard, CharacterOptionsConfiguration options)
+		{
+			Inventory = inventory ?? throw new ArgumentNullException(nameof(inventory));
+			Bank = bank ?? throw new ArgumentNullException(nameof(bank));
+			Stats = stats ?? throw new ArgumentNullException(nameof(stats));
+			Progress = progress ?? throw new ArgumentNullException(nameof(progress));
+			SpecialCustom = specialCustom ?? throw new ArgumentNullException(nameof(specialCustom));
+			Version = version ?? throw new ArgumentNullException(nameof(version));
+			Customization = customization ?? throw new ArgumentNullException(nameof(customization));
+			GuildCard = guildCard ?? throw new ArgumentNullException(nameof(guildCard));
+			Options = options ?? throw new ArgumentNullException(nameof(options));
+		}
+	}
+}
