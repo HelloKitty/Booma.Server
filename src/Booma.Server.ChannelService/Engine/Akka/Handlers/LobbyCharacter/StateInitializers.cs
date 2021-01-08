dfef00f3b1aed0d92f42d473860947ed12e52395
HelloKitty <andrew.blakely@ymail.com>
2021-01-08 05:06:27 -0600

    Added Lobby Character state initializers

---
+++ src/Booma.Server.ChannelService/Engine/Akka/Handlers/LobbyCharacter/StateInitializers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GladNet;
+using MEAKKA;
+
+namespace Booma.LobbyCharacter
+{
+	[ActorMessageHandler(typeof(LobbyCharacterActor))]
+	public sealed class InitializeInitialCharacterDataSnapshot : InitializeStateMessageHandler<InitialCharacterDataSnapshot>
+	{
+		public InitializeInitialCharacterDataSnapshot(IMutableActorState<InitialCharacterDataSnapshot> mutableStateContainer) 
+			: base(mutableStateContainer)
+		{
+
+		}
+	}
+
+	[ActorMessageHandler(typeof(LobbyCharacterActor))]
+	public sealed class InitializeMessageSender : InitializeStateMessageHandler<IMessageSendService<PSOBBGamePacketPayloadServer>>
+	{
+		public InitializeMessageSender(IMutableActorState<IMessageSendService<PSOBBGamePacketPayloadServer>> mutableStateContainer)
+			: base(mutableStateContainer)
+		{
+
+		}
+	}
+
+	[ActorMessageHandler(typeof(LobbyCharacterActor))]
+	public sealed class InitializeNetworkEntityGuid : InitializeStateMessageHandler<NetworkEntityGuid>
+	{
+		public InitializeNetworkEntityGuid(IMutableActorState<NetworkEntityGuid> mutableStateContainer)
+			: base(mutableStateContainer)
+		{
+
+		}
+	}
+}
