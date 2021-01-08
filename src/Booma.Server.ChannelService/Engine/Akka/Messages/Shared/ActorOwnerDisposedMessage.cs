10920c6c304c686e3fe1a65da9231683ec121cc3
HelloKitty <andrew.blakely@ymail.com>
2021-01-08 01:26:30 -0600

    Added ActorOwnerDisposedMessage

---
+++ src/Booma.Server.ChannelService/Engine/Akka/Messages/Shared/ActorOwnerDisposedMessage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MEAKKA;
+
+namespace Booma
+{
+	/// <summary>
+	/// Empty message indicating the owner of an actor is disposing of itself
+	/// and the actor.
+	/// </summary>
+	public sealed class ActorOwnerDisposedMessage : EntityActorMessage
+	{
+		/// <summary>
+		/// Empty message indicating the owner of an actor is disposing of itself
+		/// and the actor.
+		/// </summary>
+		public ActorOwnerDisposedMessage()
+		{
+			
+		}
+	}
+}
