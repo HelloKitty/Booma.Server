70eec2ab2fd00e11b3524a2fe0d0ffd859b41fe4
HelloKitty <andrew.blakely@ymail.com>
2021-01-08 05:05:45 -0600

    Implemented PacketSendMessageHandler

---
+++ src/Booma.Server.ChannelService/Engine/Akka/Handlers/LobbyCharacter/PacketSendMessageHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using GladNet;
+using MEAKKA;
+
+namespace Booma
+{
+	[ActorMessageHandler(typeof(LobbyCharacterActor))]
+	public sealed class PacketSendMessageHandler : BaseActorMessageHandler<SendGamePacketMessage>
+	{
+		private IActorState<IMessageSendService<PSOBBGamePacketPayloadServer>> SendService { get; }
+
+		public PacketSendMessageHandler(IActorState<IMessageSendService<PSOBBGamePacketPayloadServer>> sendService)
+		{
+			SendService = sendService ?? throw new ArgumentNullException(nameof(sendService));
+		}
+
+		public override async Task HandleMessageAsync(EntityActorMessageContext context, SendGamePacketMessage message, CancellationToken token = default)
+		{
+			await SendService.Data.SendMessageAsync(message.Packet, token);
+		}
+	}
+}
