71b0834532f106d31574edde0cbdb7c1a65d5162
HelloKitty <andrew.blakely@ymail.com>
2021-01-08 01:26:55 -0600

    Added MutableGenericActorState to service module

--- src/Booma.Server.ChannelService/Modules/MEAKKAServiceModule.cs
+++ src/Booma.Server.ChannelService/Modules/MEAKKAServiceModule.cs
@@ -37,6 +37,12 @@ namespace Booma
 				.As<ICharacterActorReferenceContainer>()
 				.OwnedByLifetimeScope()
 				.InstancePerLifetimeScope();
+
+			//This is the actor state storage objects.
+			builder.RegisterGeneric(typeof(MutableGenericActorState<>))
+				.As(typeof(IActorState<>))
+				.As(typeof(IMutableActorState<>))
+				.InstancePerLifetimeScope();
 		}
 	}
 }
