c8c74d3adaeddaf396a44cd70c65c8e7e8d350c0
HelloKitty <andrew.blakely@ymail.com>
2021-01-08 08:34:21 -0600

    Implemented BroadcastGamePacketMessage handlers

---
+++ src/Booma.Server.ChannelService/Engine/Akka/Handlers/Lobby/BroadcastGamePacketMessageHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MEAKKA;
+
+namespace Booma.Lobby
+{
+	[ActorMessageHandler(typeof(GameLobbyActor))]
+	public sealed class BroadcastGamePacketMessageHandler : BaseActorMessageHandler<BroadcastGamePacketMessage>
+	{
+		private ICharacterLobbySlotRepository LobbyCharacterRepository { get; }
+
+		public BroadcastGamePacketMessageHandler(ICharacterLobbySlotRepository lobbyCharacterRepository)
+		{
+			LobbyCharacterRepository = lobbyCharacterRepository ?? throw new ArgumentNullException(nameof(lobbyCharacterRepository));
+		}
+
+		public override async Task HandleMessageAsync(EntityActorMessageContext context, BroadcastGamePacketMessage message, CancellationToken token = new CancellationToken())
+		{
+			//TODO: make broadcasting API
+			foreach(var lobbyPlayer in await LobbyCharacterRepository.RetrieveInitializedAsync(token))
+			{
+				if(lobbyPlayer.CharacterData.EntityGuid != message.Broadcaster)
+					lobbyPlayer.Actor.Tell(new SendGamePacketMessage(message.Packet));
+			}
+		}
+	}
+}
---
+++ src/Booma.Server.ChannelService/Engine/Akka/Handlers/LobbyCharacter/BroadcastGamePacketMessageHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MEAKKA;
+
+namespace Booma.LobbyCharacter
+{
+	[ActorMessageHandler(typeof(LobbyCharacterActor))]
+	public sealed class BroadcastGamePacketMessageHandler : BaseActorMessageHandler<BroadcastGamePacketMessage>
+	{
+		public override async Task HandleMessageAsync(EntityActorMessageContext context, BroadcastGamePacketMessage message, CancellationToken token = new CancellationToken())
+		{
+			//TODO: we need to do some validation?
+			context.ActorContext
+				.Parent.Tell(message, context.Entity);
+		}
+	}
+}
