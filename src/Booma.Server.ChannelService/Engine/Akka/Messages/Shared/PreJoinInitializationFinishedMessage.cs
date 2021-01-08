4e9b9fdb5d22ccf52a03723e56731ddd71481e2f
HelloKitty <andrew.blakely@ymail.com>
2021-01-08 03:28:59 -0600

    Added PreJoinInitializationFinishedMessage

---
+++ src/Booma.Server.ChannelService/Engine/Akka/Messages/Shared/PreJoinInitializationFinishedMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MEAKKA;
+
+namespace Booma
+{
+	/// <summary>
+	/// Empty message that indicates to the actor that any pre-join initialization
+	/// logic is finished.
+	/// (Ex. Player in the process of joining a lobby has finished initializing everything required
+	/// to ask the Parent actor Lobby/Game to continue with the process of joining).
+	/// </summary>
+	public sealed class PreJoinInitializationFinishedMessage : EntityActorMessage
+	{
+		public PreJoinInitializationFinishedMessage()
+		{
+			
+		}
+	}
+}
