fb7682dce05a51f5e8dd0298218fbf983f6a6520
HelloKitty <andrew.blakely@ymail.com>
2021-01-08 04:35:59 -0600

    Added SendGamePacketMessage

---
+++ src/Booma.Server.ChannelService/Engine/Akka/Messages/Shared/SendGamePacketMessage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MEAKKA;
+
+namespace Booma
+{
+	/// <summary>
+	/// Simple message type that acts as an envelope to send a <see cref="PSOBBGamePacketPayloadServer"/> to a
+	/// target actor's network interface.
+	/// </summary>
+	public sealed class SendGamePacketMessage : EntityActorMessage
+	{
+		/// <summary>
+		/// Game packet to send to the actor's client.
+		/// </summary>
+		public PSOBBGamePacketPayloadServer Packet { get; private set; }
+
+		public SendGamePacketMessage(PSOBBGamePacketPayloadServer packet)
+		{
+			Packet = packet ?? throw new ArgumentNullException(nameof(packet));
+		}
+	}
+}
