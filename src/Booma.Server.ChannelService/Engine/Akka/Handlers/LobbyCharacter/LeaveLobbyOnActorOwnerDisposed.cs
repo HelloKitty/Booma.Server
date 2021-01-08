549b90de2de9ac05e934eb7b41a9c517095fa31e
HelloKitty <andrew.blakely@ymail.com>
2021-01-08 10:45:28 -0600

    Added actor dispose leave lobby message

---
+++ src/Booma.Server.ChannelService/Engine/Akka/Handlers/LobbyCharacter/LeaveLobbyOnActorOwnerDisposed.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MEAKKA;
+
+namespace Booma.LobbyPlayer
+{
+	public sealed class LeaveLobbyOnActorOwnerDisposed : BaseActorMessageHandler<ActorOwnerDisposedMessage>
+	{
+		private IActorState<NetworkEntityGuid> GuidState { get; }
+
+		public LeaveLobbyOnActorOwnerDisposed(IActorState<NetworkEntityGuid> guidState)
+		{
+			GuidState = guidState ?? throw new ArgumentNullException(nameof(guidState));
+		}
+
+		public override async Task HandleMessageAsync(EntityActorMessageContext context, ActorOwnerDisposedMessage message, CancellationToken token = new CancellationToken())
+		{
+			//Owner/client/session has disposed of this player actor in some fashion
+			//so we should tell the lobby (parent) that we are leaving
+			context.ActorContext.Parent
+				.Tell(new LeaveLobbyRequestMessage(GuidState.Data));
+
+			//Stops us from handling ANY more messages.
+			context.ActorContext.Stop(context.ActorContext.Self);
+		}
+	}
+}
