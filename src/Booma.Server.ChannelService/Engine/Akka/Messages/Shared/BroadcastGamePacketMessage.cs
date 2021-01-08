0f7b01ddf2d6ccb9120e9257413815e654504924
HelloKitty <andrew.blakely@ymail.com>
2021-01-08 08:33:57 -0600

    Added BroadcastGamePacketMessage actor message

---
+++ src/Booma.Server.ChannelService/Engine/Akka/Messages/Shared/BroadcastGamePacketMessage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MEAKKA;
+
+namespace Booma
+{
+	/// <summary>
+	/// Simple message type that acts as an envelope to send a <see cref="PSOBBGamePacketPayloadServer"/> to a
+	/// target actor's network interface.
+	/// </summary>
+	public sealed class BroadcastGamePacketMessage : EntityActorMessage
+	{
+		/// <summary>
+		/// Game packet to broadcast.
+		/// </summary>
+		public PSOBBGamePacketPayloadServer Packet { get; private set; }
+
+		/// <summary>
+		/// Entity responsible for broadcasting.
+		/// </summary>
+		public NetworkEntityGuid Broadcaster { get; private set; }
+
+		public BroadcastGamePacketMessage(PSOBBGamePacketPayloadServer packet, NetworkEntityGuid broadcaster)
+		{
+			Packet = packet ?? throw new ArgumentNullException(nameof(packet));
+			Broadcaster = broadcaster ?? throw new ArgumentNullException(nameof(broadcaster));
+		}
+	}
+}
