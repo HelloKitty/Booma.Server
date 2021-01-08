c9ebd46bf076fcb99b266dd89db1e8c2487867b1
HelloKitty <andrew.blakely@ymail.com>
2021-01-08 10:45:15 -0600

    Added LeaveLobbyRequestMessage

---
+++ src/Booma.Server.ChannelService/Engine/Akka/Messages/Lobby/LeaveLobbyRequestMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+using MEAKKA;
+
+namespace Booma
+{
+	public sealed class LeaveLobbyRequestMessage : EntityActorMessage
+	{
+		/// <summary>
+		/// The entity requesting to leave the lobby.
+		/// </summary>
+		public NetworkEntityGuid Entity { get; private set; }
+
+		public LeaveLobbyRequestMessage(NetworkEntityGuid entity)
+		{
+			Entity = entity ?? throw new ArgumentNullException(nameof(entity));
+		}
+	}
+}
