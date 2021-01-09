2bfd7252d686f54632d02dc057d0ad09e1e3b959
HelloKitty <andrew.blakely@ymail.com>
2021-01-09 14:21:50 -0600

    Fixed and refactored lobby change request handler

--- src/Booma.Server.ChannelService/Network/Handlers/BlockLobbyChangeRequestMessageHandler.cs
+++ src/Booma.Server.ChannelService/Network/Handlers/BlockLobbyChangeRequestMessageHandler.cs
@@ -29,28 +29,34 @@ namespace Booma
 			//Dispose of the current actor, we're gonna be leaving the lobby.
 			await ActorContainer.DisposeAsync();
 
+			//If this fails then All lobbies are full or failed to join any.
+			if(!await TrySwitchLobbyAsync(context, token, lobbyId))
+				await context.ConnectionService.DisconnectAsync();
+		}
+
+		private async Task<bool> TrySwitchLobbyAsync(SessionMessageContext<PSOBBGamePacketPayloadServer> context, CancellationToken token, int lobbyId)
+		{
 			//Good chance the lobby will be full.
-			if (!await LobbyEntryService.TryEnterLobbyAsync(context, ActorContainer.EntityGuid, lobbyId, token))
-			{
-				await context.MessageService.SendMessageAsync(new SharedCreateMessageBoxEventPayload($"Failed to join the requested lobby. Will attempt to find a replacement lobby."), token);
-				await Task.Delay(5000, token);
+			if (await LobbyEntryService.TryEnterLobbyAsync(context, ActorContainer.EntityGuid, lobbyId, token))
+				return true;
 
-				//Been 5 seconds, we can retry before we select for them.
-				if (!await LobbyEntryService.TryEnterLobbyAsync(context, ActorContainer.EntityGuid, lobbyId, token))
+			await context.MessageService.SendMessageAsync(new SharedCreateMessageBoxEventPayload($"Failed to join the requested lobby. Will attempt to find a replacement lobby."), token);
+			await Task.Delay(5000, token);
+
+			//Been 5 seconds, we can retry before we select for them.
+			if(await LobbyEntryService.TryEnterLobbyAsync(context, ActorContainer.EntityGuid, lobbyId, token))
+				return true;
+
+			//TODO: Better handle try join ANY lobby.
+			//We don't know what lobby they were in, so let's try to put them in the first lobby and go down the list.
+			for(int i = 0; i < 15; i++)
+				if(await LobbyEntryService.TryEnterLobbyAsync(context, ActorContainer.EntityGuid, i, token))
 				{
-					//TODO: Better handle try join ANY lobby.
-					//We don't know what lobby they were in, so let's try to put them in the first lobby and go down the list.
-					for(int i = 0; i < 15; i++)
-						if(await LobbyEntryService.TryEnterLobbyAsync(context, ActorContainer.EntityGuid, i, token))
-						{
-							//Case where we ended up actually getting into the requested lobby.
-							return;
-						}
+					//Case where we ended up actually getting into the requested lobby.
+					return true;
 				}
-			}
 
-			//All lobbies are full or failed to join any.
-			await context.ConnectionService.DisconnectAsync();
+			return false;
 		}
 	}
 }
