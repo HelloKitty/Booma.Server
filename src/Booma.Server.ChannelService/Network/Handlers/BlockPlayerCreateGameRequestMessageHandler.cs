0b64c7a7e6596458646a88c0ffcf1d2310b2c44d
HelloKitty <andrew.blakely@ymail.com>
2021-01-20 18:32:13 -0600

    Added demo game join

---
+++ src/Booma.Server.ChannelService/Network/Handlers/BlockPlayerCreateGameRequestMessageHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using GladNet;
+
+namespace Booma
+{
+	public sealed class BlockPlayerCreateGameRequestMessageHandler : GameMessageHandler<BlockPlayerCreateGameRequestPayload>
+	{
+		public override async Task HandleMessageAsync(SessionMessageContext<PSOBBGamePacketPayloadServer> context, BlockPlayerCreateGameRequestPayload message, CancellationToken token = default)
+		{
+			await context.MessageService
+				.SendMessageAsync(new BlockGameJoinEventPayload(0, 0, new GameSettings(DifficultyType.Normal, false, 0, SectionId.Viridia, false, 0, EpisodeType.EpisodeI, false), new PlayerInformationHeader[0]), token);
+		}
+	}
+}
