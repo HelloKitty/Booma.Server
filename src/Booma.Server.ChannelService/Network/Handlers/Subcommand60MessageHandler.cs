eb5ce488d1c86911c13f43c248dc70d670cadb48
HelloKitty <andrew.blakely@ymail.com>
2021-03-12 09:00:39 -0600

    Temp disable sub60 dead letter disconnection

--- src/Booma.Server.ChannelService/Network/Handlers/Subcommand60MessageHandler.cs
+++ src/Booma.Server.ChannelService/Network/Handlers/Subcommand60MessageHandler.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Akka.Actor;
+using Common.Logging;
 using GladNet;
 using MEAKKA;
 
@@ -20,15 +21,21 @@ namespace Booma
 		/// </summary>
 		private ICharacterActorReferenceContainer ActorReference { get; }
 
-		public Subcommand60MessageHandler(ICharacterActorReferenceContainer actorReference) 
+		private ILog Logger { get; }
+
+		public Subcommand60MessageHandler(ICharacterActorReferenceContainer actorReference, ILog logger)
 		{
 			ActorReference = actorReference ?? throw new ArgumentNullException(nameof(actorReference));
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
 		}
 
 		public override async Task HandleMessageAsync(SessionMessageContext<PSOBBGamePacketPayloadServer> context, BlockNetworkCommand60EventClientPayload message, CancellationToken token = new CancellationToken())
 		{
-			if(!ActorReference.IsAvailable)
-				await context.ConnectionService.DisconnectAsync();
+			//This is TEMP disabled until we have implemented Lobby -> Game or Game -> Lobby Actor creation
+			/*if(!ActorReference.IsAvailable)
+				await context.ConnectionService.DisconnectAsync();*/
+			if (!ActorReference.IsAvailable && Logger.IsErrorEnabled)
+				Logger.Error($"Unexpected DEADLETTER due to no valid actor reference. TODO: We need to make sure this never occurs and disconnect one day.");
 
 			//TODO: Validate packet data!
 			ActorReference.Reference.TellSelf(new BroadcastGamePacketMessage(new BlockNetworkCommand60EventServerPayload(message.Command), ActorReference.EntityGuid));
