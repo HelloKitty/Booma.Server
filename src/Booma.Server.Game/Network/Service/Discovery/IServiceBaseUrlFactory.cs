22e8a85bc4b5df339d52762b40bffb23d36d22a7
HelloKitty <andrew.blakely@ymail.com>
2021-03-18 22:06:24 -0500

    Added missing IServiceBaseUrlFactory

---
+++ src/Booma.Server.Game/Network/Service/Discovery/IServiceBaseUrlFactory.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using Glader.ASP.ServiceDiscovery;
+using Glader.Essentials;
+using JetBrains.Annotations;
+
+namespace Booma
+{
+	public interface IServiceBaseUrlFactory : IFactoryCreatable<string, ResolvedEndpoint>
+	{
+
+	}
+}
