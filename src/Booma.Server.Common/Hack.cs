a2715f6274c2e655604bfe849c0cdad815739b96
HelloKitty <andrew.blakely@ymail.com>
2021-03-18 22:05:53 -0500

    Added C# 9 Hack file

---
+++ src/Booma.Server.Common/Hack.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//See: https://stackoverflow.com/questions/64749385/predefined-type-system-runtime-compilerservices-isexternalinit-is-not-defined
+namespace System.Runtime.CompilerServices
+{
+	public static class IsExternalInit { }
+}
