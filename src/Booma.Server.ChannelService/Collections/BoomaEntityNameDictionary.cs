44717c0cb9c336c315be9f682a9876c283024196
HelloKitty <andrew.blakely@ymail.com>
2021-03-19 01:42:27 -0500

    Added BoomaEntityNameDictionary

---
+++ src/Booma.Server.ChannelService/Collections/BoomaEntityNameDictionary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.ASP.NameQuery;
+using Glader.Essentials;
+
+namespace Booma
+{
+	public interface IBoomaEntityNameDictionary : IEntityNameDictionary<NetworkEntityGuid, EntityType>
+	{
+
+	}
+
+	public sealed class BoomaEntityNameDictionary : CachedEntityNameDictionary<NetworkEntityGuid, EntityType>, IBoomaEntityNameDictionary
+	{
+
+	}
+}
