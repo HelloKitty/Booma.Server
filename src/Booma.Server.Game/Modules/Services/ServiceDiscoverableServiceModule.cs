d4f15aed781c7a26a6a1100302585750e5bd011b
HelloKitty <andrew.blakely@ymail.com>
2021-03-18 20:50:58 -0500

    Added ServiceDiscoveryModule mode

--- src/Booma.Server.Game/Modules/Services/ServiceDiscoverableServiceModule.cs
+++ src/Booma.Server.Game/Modules/Services/ServiceDiscoverableServiceModule.cs
@@ -9,6 +9,12 @@ using Glader.Essentials;
 
 namespace Booma
 {
+	public enum ServiceDiscoveryModuleMode
+	{
+		Default = 1,
+		Authorized = 2
+	}
+
 	/// <summary>
 	/// Service module that registers a <see cref="IServiceResolver{TServiceInterfaceType}"/> for the specified
 	/// <typeparamref name="TServiceType"/>. Using <see cref="IServiceDiscoveryService"/> to resolve them.
@@ -22,10 +28,13 @@ namespace Booma
 		/// </summary>
 		private BoomaServiceType ServiceType { get; }
 
-		public ServiceDiscoverableServiceModule(BoomaServiceType serviceType)
+		public ServiceDiscoveryModuleMode Mode { get; }
+
+		public ServiceDiscoverableServiceModule(BoomaServiceType serviceType, ServiceDiscoveryModuleMode mode = ServiceDiscoveryModuleMode.Authorized)
 		{
 			if (!Enum.IsDefined(typeof(BoomaServiceType), serviceType)) throw new InvalidEnumArgumentException(nameof(serviceType), (int) serviceType, typeof(BoomaServiceType));
 			ServiceType = serviceType;
+			Mode = mode;
 		}
 
 		/// <inheritdoc />
@@ -33,15 +42,27 @@ namespace Booma
 		{
 			base.Load(builder);
 
-			//I know this seems strange, but having the service provider be unique to the
-			//lifetimescope means we can inject Auth tokens into it and not worry about sharing this resource.
-			//It also means if a service dies or gets removed, reconnecting will yield another service on connection.
-			builder.Register<ServiceDiscoveryServiceResolver<TServiceType>>(context =>
-				{
-					return new ServiceDiscoveryServiceResolver<TServiceType>(context.Resolve<IServiceDiscoveryService>(), ServiceType, context.Resolve<ILog>(), context.Resolve<IReadonlyAuthTokenRepository>());
-				})
-				.As<IServiceResolver<TServiceType>>()
-				.InstancePerLifetimeScope();
+			if (Mode == ServiceDiscoveryModuleMode.Authorized)
+			{
+				//I know this seems strange, but having the service provider be unique to the
+				//lifetimescope means we can inject Auth tokens into it and not worry about sharing this resource.
+				//It also means if a service dies or gets removed, reconnecting will yield another service on connection.
+				builder.Register<AuthorizedServiceDiscoveryServiceResolver<TServiceType>>(context =>
+					{
+						return new AuthorizedServiceDiscoveryServiceResolver<TServiceType>(context.Resolve<IServiceDiscoveryService>(), ServiceType, context.Resolve<ILog>(), context.Resolve<IReadonlyAuthTokenRepository>());
+					})
+					.As<IServiceResolver<TServiceType>>()
+					.InstancePerLifetimeScope();
+			}
+			else
+			{
+				builder.Register<DefaultServiceDiscoveryServiceResolver<TServiceType>>(context =>
+					{
+						return new DefaultServiceDiscoveryServiceResolver<TServiceType>(context.Resolve<IServiceDiscoveryService>(), ServiceType, context.Resolve<ILog>());
+					})
+					.As<IServiceResolver<TServiceType>>()
+					.InstancePerLifetimeScope();
+			}
 		}
 	}
 }
