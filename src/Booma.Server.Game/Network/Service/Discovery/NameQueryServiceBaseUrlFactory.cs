64e93c1ba92a2dc7e610d7c4fc209b7c0caea0a8
HelloKitty <andrew.blakely@ymail.com>
2021-03-19 01:42:05 -0500

    Added NameQueryServiceBaseUrlFactory

---
+++ src/Booma.Server.Game/Network/Service/Discovery/NameQueryServiceBaseUrlFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Glader.ASP.ServiceDiscovery;
+
+namespace Booma
+{
+	public sealed class NameQueryServiceBaseUrlFactory : IServiceBaseUrlFactory
+	{
+		public EntityType EntityTypeValue { get; }
+
+		public DefaultServiceBaseUrlFactory DefaultUrlFactory { get; } = new DefaultServiceBaseUrlFactory();
+
+		public NameQueryServiceBaseUrlFactory(EntityType entityTypeValue)
+		{
+			if(!Enum.IsDefined(typeof(EntityType), entityTypeValue)) throw new InvalidEnumArgumentException(nameof(entityTypeValue), (int)entityTypeValue, typeof(EntityType));
+
+			EntityTypeValue = entityTypeValue;
+		}
+
+		public string Create(ResolvedEndpoint context)
+		{
+			if (context == null) throw new ArgumentNullException(nameof(context));
+
+			//TODO: Refactor path building to NameQuery library
+			return $"{DefaultUrlFactory.Create(context)}/api/{EntityTypeValue}Name";
+		}
+	}
+}
