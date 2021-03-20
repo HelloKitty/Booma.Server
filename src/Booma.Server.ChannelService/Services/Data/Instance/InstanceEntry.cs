10f1c39b99e93b7424fdd2d357842b837c531a6a
HelloKitty <andrew.blakely@ymail.com>
2021-03-20 13:44:42 -0500

    Added mising InstanceEntry

---
+++ src/Booma.Server.ChannelService/Services/Data/Instance/InstanceEntry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Booma
+{
+	/// <summary>
+	/// Simple data model for a instance entry.
+	/// </summary>
+	public sealed class InstanceEntry
+	{
+		/// <summary>
+		/// The id of the instance entry.
+		/// </summary>
+		public int InstanceId { get; }
+
+		//TODO: Should we expose this??
+		/// <summary>
+		/// The path to the instance actor.
+		/// </summary>
+		public string InstanceActorPath { get; }
+
+		public InstanceEntry(int instanceId, string lobbyActorPath)
+		{
+			if (instanceId < 0) throw new ArgumentOutOfRangeException(nameof(instanceId));
+
+			InstanceId = instanceId;
+			InstanceActorPath = lobbyActorPath ?? throw new ArgumentNullException(nameof(lobbyActorPath));
+		}
+	}
+}
