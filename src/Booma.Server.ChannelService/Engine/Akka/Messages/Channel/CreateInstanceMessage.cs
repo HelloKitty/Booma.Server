f51b8ff59afea98696e271de23b5496e21e6b6f8
HelloKitty <andrew.blakely@ymail.com>
2021-03-20 13:44:03 -0500

    Added CreateInstanceMessage

---
+++ src/Booma.Server.ChannelService/Engine/Akka/Messages/Channel/CreateInstanceMessage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MEAKKA;
+
+namespace Booma
+{
+	/// <summary>
+	/// Request message to create an instance.
+	/// </summary>
+	public sealed class CreateInstanceMessage : EntityActorMessage
+	{
+		/// <summary>
+		/// The id of the requested instance.
+		/// (Will likely be the GroupId/PartyId in PSO)
+		/// </summary>
+		public int InstanceId { get; }
+
+		public CreateInstanceMessage(int instanceId)
+		{
+			if (instanceId <= 0) throw new ArgumentOutOfRangeException(nameof(instanceId));
+
+			InstanceId = instanceId;
+		}
+	}
+}
