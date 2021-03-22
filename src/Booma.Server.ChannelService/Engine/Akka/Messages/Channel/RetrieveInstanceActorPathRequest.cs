e14c62999c7fe8e3bced1d750a20dfcf3de8caec
HelloKitty <andrew.blakely@ymail.com>
2021-03-21 19:52:39 -0500

    Added RetrieveInstanceActorPathRequest actor message

---
+++ src/Booma.Server.ChannelService/Engine/Akka/Messages/Channel/RetrieveInstanceActorPathRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+using MEAKKA;
+
+namespace Booma
+{
+	public enum RetrieveInstanceActorPathResponseCode
+	{
+		Success = GladerEssentialsModelConstants.RESPONSE_CODE_SUCCESS_VALUE,
+		GeneralServerError = 2,
+		UnknownInstance = 3
+	}
+
+	public sealed class RetrieveInstanceActorPathRequest : EntityActorMessage, IActorRequestMessage<ResponseModel<string, RetrieveInstanceActorPathResponseCode>>
+	{
+		public int InstanceId { get; init; }
+	}
+}
