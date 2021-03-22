701b6d2ac0ca3a0558a72c1ffdce22b6323cefa5
HelloKitty <andrew.blakely@ymail.com>
2021-03-21 21:51:24 -0500

    Added PlayerJoinInstanceRequest

---
+++ src/Booma.Server.ChannelService/Engine/Akka/Messages/Instance/PlayerJoinInstanceRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+using MEAKKA;
+
+namespace Booma
+{
+	public enum PlayerJoinInstanceResponseCode
+	{
+		Success = GladerEssentialsModelConstants.RESPONSE_CODE_SUCCESS_VALUE,
+		GeneralServerError = 2,
+		InstanceFull = 3,
+		NotAuthorized = 4,
+	}
+
+	/// <summary>
+	/// Actor message sent by a player to join an instance.
+	/// Response returns <see cref="PlayerJoinInstanceResponseCode"/> and the player's created Actor path.
+	/// </summary>
+	public sealed class PlayerJoinInstanceRequest : EntityActorMessage, IActorRequestMessage<ResponseModel<string, PlayerJoinInstanceResponseCode>>
+	{
+		/// <summary>
+		/// The guid of the player trying to join.
+		/// </summary>
+		public NetworkEntityGuid PlayerGuid { get; init; }
+	}
+}
