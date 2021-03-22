84b33a7ebf5fe190c34d710369add0ece53e5166
HelloKitty <andrew.blakely@ymail.com>
2021-03-22 13:49:29 -0500

    Added PostInitializeActorMessage

---
+++ src/Booma.Server.ChannelService/Engine/Akka/Messages/Shared/PostInitializeActorMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MEAKKA;
+
+namespace Booma
+{
+	/// <summary>
+	/// Empty message that indicates to the actor to run post-initialization logic.
+	/// </summary>
+	public sealed class PostInitializeActorMessage : EntityActorMessage
+	{
+
+	}
+}
