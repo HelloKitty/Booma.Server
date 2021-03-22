c2f0a760e21f856ac2645e9bfc61be64a41a8fef
HelloKitty <andrew.blakely@ymail.com>
2021-03-21 19:53:06 -0500

    Added RetrieveInstanceActorPathRequestMessageHandler

---
+++ src/Booma.Server.ChannelService/Engine/Akka/Handlers/Channel/RetrieveInstanceActorPathRequestMessageHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Glader.Essentials;
+using MEAKKA;
+
+namespace Booma
+{
+	/// <summary>
+	/// Handler for mostly <see cref="RootChannelActor"/> which handles instance path GET requests.
+	/// </summary>
+	[ActorMessageHandler(typeof(RootChannelActor))]
+	public sealed class RetrieveInstanceActorPathRequestMessageHandler : ActorRequestMessageHandler<RetrieveInstanceActorPathRequest, ResponseModel<string, RetrieveInstanceActorPathResponseCode>>
+	{
+		/// <summary>
+		/// Repository for instances.
+		/// </summary>
+		private IInstanceEntryRepository InstanceRepository { get; }
+
+		public RetrieveInstanceActorPathRequestMessageHandler(IInstanceEntryRepository instanceRepository)
+		{
+			InstanceRepository = instanceRepository ?? throw new ArgumentNullException(nameof(instanceRepository));
+		}
+
+		protected override async Task<ResponseModel<string, RetrieveInstanceActorPathResponseCode>> HandleRequestAsync(EntityActorMessageContext context, RetrieveInstanceActorPathRequest message, CancellationToken token = default)
+		{
+			if (!await InstanceRepository.ContainsAsync(message.InstanceId, token))
+				return new ResponseModel<string, RetrieveInstanceActorPathResponseCode>(RetrieveInstanceActorPathResponseCode.UnknownInstance);
+
+			var instance = await InstanceRepository.RetrieveAsync(message.InstanceId, token);
+
+			return new ResponseModel<string, RetrieveInstanceActorPathResponseCode>(instance.InstanceActorPath);
+		}
+	}
+}
