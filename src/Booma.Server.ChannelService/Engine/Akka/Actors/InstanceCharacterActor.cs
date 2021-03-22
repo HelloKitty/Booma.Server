b99f8cb8d2815de1510e39915bc6fc2e19f567a8
HelloKitty <andrew.blakely@ymail.com>
2021-03-21 21:15:32 -0500

    Added InstanceCharacterActor

---
+++ src/Booma.Server.ChannelService/Engine/Akka/Actors/InstanceCharacterActor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Common.Logging;
+using Glader.Essentials;
+using MEAKKA;
+
+namespace Booma
+{
+	public sealed class InstanceCharacterActor : BaseEntityActor<EmptyFactoryContext>
+	{
+		//We have to use specific type because it contains actor type information.
+		public InstanceCharacterActor(ILog logger, DefaultActorMessageHandlerService<InstanceCharacterActor> messageHandlerService)
+			: base(logger, messageHandlerService)
+		{
+
+		}
+
+		protected override void OnInitialized(EntityActorInitializationSuccessMessage successMessage)
+		{
+			if(Logger.IsInfoEnabled)
+				Logger.Info($"{nameof(InstanceCharacterActor)} Initialized. Path: {Self.Path}");
+		}
+	}
+}
