f1572d0c92ad63876eefe9d7a3e94c216554113a
HelloKitty <andrew.blakely@ymail.com>
2021-03-22 13:50:20 -0500

    Added LobbyPostInitializeActorMessageHandler

---
+++ src/Booma.Server.ChannelService/Engine/Akka/Handlers/Lobby/LobbyPostInitializeActorMessageHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MEAKKA;
+
+namespace Booma.Lobby
+{
+	[ActorMessageHandler(typeof(LobbyActor))]
+	public sealed class LobbyPostInitializeActorMessageHandler : BaseActorMessageHandler<PostInitializeActorMessage>
+	{
+		/// <inheritdoc />
+		public override async Task HandleMessageAsync(EntityActorMessageContext context, PostInitializeActorMessage message, CancellationToken token = default)
+		{
+			//This is enable the broadcast group IActorMessageBroadcaster<LobbyActorGroupType>
+			context.Entity.InitializeState<IActorMessageBroadcaster<LobbyActorGroupType>>(new DefaultGenericActorMessageBroadcaster<LobbyActorGroupType>(context.ActorContext));
+		}
+	}
+}
