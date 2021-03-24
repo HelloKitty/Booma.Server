05c0ce603c2b4b8e4dd6ca516e0845e5bc495350
HelloKitty <andrew.blakely@ymail.com>
2021-03-23 23:23:33 -0500

    Added PostInitializeActorMessageHandler for InstanceCharacter

---
+++ src/Booma.Server.ChannelService/Engine/Akka/Handlers/InstanceCharacter/PostInitializeActorMessageHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MEAKKA;
+
+namespace Booma.InstanceCharacter
+{
+	[ActorMessageHandler(typeof(InstanceCharacterActor))]
+	public sealed class PostInitializeActorMessageHandler : BaseActorMessageHandler<PostInitializeActorMessage>
+	{
+		private IActorState<NetworkEntityGuid> ActorGuid { get; }
+
+		public PostInitializeActorMessageHandler(IActorState<NetworkEntityGuid> actorGuid)
+		{
+			ActorGuid = actorGuid ?? throw new ArgumentNullException(nameof(actorGuid));
+		}
+
+		/// <inheritdoc />
+		public override async Task HandleMessageAsync(EntityActorMessageContext context, PostInitializeActorMessage message, CancellationToken token = default)
+		{
+			//Tell the parent instance actor that we want to join the world now
+			context.ActorContext.Parent.TellEntity(new JoinWorldRequestMessage(ActorGuid.Data));
+		}
+	}
+}
