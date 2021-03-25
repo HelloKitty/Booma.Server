8a2891abd4870e21349a4e6d778154242f4ec364
HelloKitty <andrew.blakely@ymail.com>
2021-03-24 22:32:49 -0500

    Added IWorldSlotDefinition

---
+++ src/Booma.Server.ChannelService/Services/Data/Shared/IWorldSlotDefinition.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Akka.Actor;
+using MEAKKA;
+
+namespace Booma
+{
+	public interface IWorldSlotDefinition
+	{
+		/// <summary>
+		/// Slot number.
+		/// </summary>
+		public int Slot { get; }
+
+		/// <summary>
+		/// The actor reference for the slot.
+		/// </summary>
+		public IActorRef Actor { get; }
+
+		/// <summary>
+		/// Indicates if the character is initialized.
+		/// (don't send messages to an uninitialized character reference)
+		/// </summary>
+		bool IsInitialized { get; set; }
+	}
+}
