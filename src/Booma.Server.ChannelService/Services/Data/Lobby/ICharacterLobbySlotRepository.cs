1c1a645c7c6439a40e78a2e098566ef0877d5f4a
HelloKitty <andrew.blakely@ymail.com>
2021-03-24 22:33:10 -0500

    Reimplemented ICharacterLobbySlotRepository as IBaseSlotRepository

--- src/Booma.Server.ChannelService/Services/Data/Lobby/ICharacterLobbySlotRepository.cs
+++ src/Booma.Server.ChannelService/Services/Data/Lobby/ICharacterLobbySlotRepository.cs
@@ -7,43 +7,8 @@ using Glader.Essentials;
 
 namespace Booma
 {
-	public interface ICharacterLobbySlotRepository : IGenericRepositoryCrudable<int, CharacterLobbySlot>, IEntireTableQueryable<CharacterLobbySlot>
+	public interface ICharacterLobbySlotRepository : IBaseSlotRepository<CharacterLobbySlot>
 	{
-		/// <summary>
-		/// Indicates if a slot is available in the lobby.
-		/// </summary>
-		/// <param name="token">Cancel token.</param>
-		/// <returns>True if a slot is available.</returns>
-		Task<bool> HasAvailableSlotAsync(CancellationToken token = default);
-
-		/// <summary>
-		/// Indicates the first available slot value for the character lobby slot.
-		/// </summary>
-		/// <param name="token">Cancel token.</param>
-		/// <returns>Available slot.</returns>
-		Task<int> FirstAvailableSlotAsync(CancellationToken token = default);
-
-		/// <summary>
-		/// Indicates if the lobby contains an assigned slot for the provided entity.
-		/// </summary>
-		/// <param name="messageEntity">Entity to look for.</param>
-		/// <param name="token">The cancel token.</param>
-		/// <returns>True if the lobby contains a slot assigned to the entity.</returns>
-		Task<bool> ContainsEntitySlotAsync(NetworkEntityGuid messageEntity, CancellationToken token = default);
-
-		/// <summary>
-		/// Retrieves a lobby entry by the <see cref="NetworkEntityGuid"/>.
-		/// </summary>
-		/// <param name="guid">The entity guid.</param>
-		/// <param name="token">Cancel token.</param>
-		/// <returns></returns>
-		Task<CharacterLobbySlot> RetrieveAsync(NetworkEntityGuid guid, CancellationToken token = default);
-
-		/// <summary>
-		/// Retrieves all initialized lobby entries.
-		/// </summary>
-		/// <param name="token">Cancel token.</param>
-		/// <returns></returns>
-		Task<IEnumerable<CharacterLobbySlot>> RetrieveInitializedAsync(CancellationToken token = default);
+		
 	}
 }
