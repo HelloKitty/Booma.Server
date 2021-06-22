0c1363c50ea7ba61fab0f92dcc9211cfa23bf23a
HelloKitty <andrew.blakely@ymail.com>
2021-06-21 23:02:22 -0500

    Make GGDBFInitializer reinitializable

--- src/Booma.Server.ChannelService/Services/Data/GGDBFInitializer.cs
+++ src/Booma.Server.ChannelService/Services/Data/GGDBFInitializer.cs
@@ -21,8 +21,6 @@ namespace Booma
 
 		private AsyncLock Lock { get; } = new AsyncLock();
 
-		private bool Loaded { get; set; } = false;
-
 		public GGDBFInitializer(IServiceDiscoveryService serviceDiscoveryClient, ILog logger)
 		{
 			ServiceDiscoveryClient = serviceDiscoveryClient ?? throw new ArgumentNullException(nameof(serviceDiscoveryClient));
@@ -31,35 +29,26 @@ namespace Booma
 
 		public async Task InitializeAsync(CancellationToken token = default)
 		{
-			if (!Loaded)
+			using (await Lock.LockAsync())
 			{
-				using (await Lock.LockAsync())
+				var endpointResponse = await ServiceDiscoveryClient.DiscoverServiceAsync(BoomaEndpointConstants.GetServiceIdentifier(BoomaServiceType.CharacterDataService), token);
+
+				if(endpointResponse == null)
+					throw new InvalidOperationException($"Failed to retrieve endpoint for ServiceType: {BoomaServiceType.CharacterDataService}");
+
+				var dataSource = new RefitHttpGGDBFDataSource<RPGStaticDataContext<DefaultTestSkillType, CharacterRace, CharacterClass, PsobbProportionSlots, PsobbCustomizationSlots, CharacterStatType>>($"{endpointResponse.Result.Address}:{endpointResponse.Result.Port}", new RefitHttpGGDBFDataSourceOptions()
 				{
-					//Double check locking
-					if (Loaded)
-						return;
-
-					var endpointResponse = await ServiceDiscoveryClient.DiscoverServiceAsync(BoomaEndpointConstants.GetServiceIdentifier(BoomaServiceType.CharacterDataService), token);
-
-					if(endpointResponse == null)
-						throw new InvalidOperationException($"Failed to retrieve endpoint for ServiceType: {BoomaServiceType.CharacterDataService}");
-
-					var dataSource = new RefitHttpGGDBFDataSource<RPGStaticDataContext<DefaultTestSkillType, CharacterRace, CharacterClass, PsobbProportionSlots, PsobbCustomizationSlots, CharacterStatType>>($"{endpointResponse.Result.Address}:{endpointResponse.Result.Port}", new RefitHttpGGDBFDataSourceOptions()
+					RefreshOnFirstQuery = true,
+					Settings = new RefitSettings()
 					{
-						RefreshOnFirstQuery = false,
-						Settings = new RefitSettings()
-						{
-							HttpMessageHandlerFactory = () => new BypassHttpsValidationHandler()
-						}
-					});
+						HttpMessageHandlerFactory = () => new BypassHttpsValidationHandler()
+					}
+				});
 
-					await RPGStaticDataContext<DefaultTestSkillType, CharacterRace, CharacterClass, PsobbProportionSlots, PsobbCustomizationSlots, CharacterStatType>.Initialize(dataSource);
+				await RPGStaticDataContext<DefaultTestSkillType, CharacterRace, CharacterClass, PsobbProportionSlots, PsobbCustomizationSlots, CharacterStatType>.Initialize(dataSource);
 
-					if(Logger.IsInfoEnabled)
-						Logger.Info($"GGDBF Initialized");
-
-					Loaded = true;
-				}
+				if(Logger.IsInfoEnabled)
+					Logger.Info($"GGDBF Initialized");
 			}
 		}
 	}
