1a9d95c5f765f77b9534f14331a6aa451fb14d0a
HelloKitty <andrew.blakely@ymail.com>
2021-12-17 03:02:12 -0600

    Added JSON to DB Population logic for Frames Items

---
+++ src/Tools/Booma.Tools.ItemPMT.JSON.DatabasePopulator/Models/ItemPMTEditor/ItemPMTFrames.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Booma.Tools.ItemPMT.JSON.Frames
+{
+	// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+	public class List
+	{
+		public string name { get; set; }
+		public string desc { get; set; }
+		public int stars { get; set; }
+		public int announce { get; set; }
+		public bool qreward { get; set; }
+		public int id { get; set; }
+		public int model { get; set; }
+		public int texture { get; set; }
+		public int teamPoints { get; set; }
+		public int dfp { get; set; }
+		public int evp { get; set; }
+		public int blockParticle { get; set; }
+		public int blockEffect { get; set; }
+		public int _class { get; set; }
+		public int level { get; set; }
+		public int efr { get; set; }
+		public int eth { get; set; }
+		public int eic { get; set; }
+		public int edk { get; set; }
+		public int elt { get; set; }
+		public int dfpRange { get; set; }
+		public int evpRange { get; set; }
+		public int statBoost { get; set; }
+		public int techBoost { get; set; }
+		public int unknown1 { get; set; }
+		public int unknown2 { get; set; }
+	}
+
+	public class Frames
+	{
+		public double sale { get; set; }
+		public List<List> list { get; set; }
+	}
+
+	public class ItemPMTFrames
+	{
+		public Frames frames { get; set; }
+	}
+}
--- src/Tools/Booma.Tools.ItemPMT.JSON.DatabasePopulator/Program.cs
+++ src/Tools/Booma.Tools.ItemPMT.JSON.DatabasePopulator/Program.cs
@@ -3,6 +3,8 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Booma.Server.CharacterDataService;
+using Booma.Tools.ItemPMT.JSON.Frames;
+using Booma.Tools.ItemPMT.JSON.Weapons;
 using Fasterflect;
 using Glader.ASP.RPG;
 using Microsoft.EntityFrameworkCore;
@@ -26,38 +28,73 @@ namespace Booma.Tools.ItemPMT.JSON.DatabasePopulator
 				var itemTemplateSet = context.Set<DBRPGItemTemplate<ItemClassType, PsobbQuality, Vector3<byte>>>();
 				var itemSubclassSet = context.Set<DBRPGSItemSubClass<ItemClassType>>();
 
-				//TODO: Remove absolute paths
-				//Load weapon data
-				var weaponData = JsonConvert.DeserializeObject<ItemPMTWeapons>(File.ReadAllText($@"C:\Users\Glader\Documents\Github\Booma.Server\Data\JSON\ItemPMT.Weapons.json"));
+				await LoadWeaponEntriesAsync(itemTemplateSet, itemSubclassSet);
 
-				//TODO: We're skipping SubClass 0 even though PSO does have some data in this group (for weapons its nonsense)
-				for (int groupId = 1; groupId < weaponData.weapons.group.Count; groupId++)
+				var frameData = JsonConvert.DeserializeObject<ItemPMTFrames>(File.ReadAllText($@"C:\Users\Glader\Documents\Github\Booma.Server\Data\JSON\ItemPMT.Frames.json"));
+
+				for (int i = 0; i < frameData.frames.list.Count; i++)
 				{
-					for (int i = 0; i < weaponData.weapons.group[groupId].list.Count; i++)
-					{
-						var entry = weaponData.weapons.group[groupId].list[i];
-						int key = (groupId << (1 * sizeof(byte) * 8)) + i; //Key: Group:G Index:I 0xGGII
+					var entry = frameData.frames.list[i];
+					var key = CreateItemTemplateKey(ItemClassType.Guard, 1, i); //Subclass 1 is Frame
 
-						//If it's already inserted don't reinsert
-						if ((await itemTemplateSet.FindAsync(key)) != null)
-							continue;
+					//If it's already inserted don't reinsert
+					if((await itemTemplateSet.FindAsync(key)) != null)
+						continue;
 
-						//First we must consider also that the subclass may not exist.
-						if (await itemSubclassSet.FindAsync(ItemClassType.Weapon, groupId) == null)
-						{
-							var firstEntryInGroup = weaponData.weapons.group[groupId].list.FirstOrDefault();
-							itemSubclassSet.Add(new DBRPGSItemSubClass<ItemClassType>(groupId, ItemClassType.Weapon, $"TODO: {(firstEntryInGroup == null ? "Unk" : firstEntryInGroup.name)}", "TODO"));
-						}
+					await CreateSubclassIfNeededAsync(ItemClassType.Guard, itemSubclassSet, 1, "Frame");
 
-						var dbEntry = new DBRPGItemTemplate<ItemClassType, PsobbQuality, Vector3<byte>>(ItemClassType.Weapon, groupId, entry.name, entry.desc, PsobbQuality.Common);
-						dbEntry.TrySetPropertyValue(nameof(DBRPGItemTemplate<ItemClassType, PsobbQuality, Vector3<byte>>.Id), key);
-						itemTemplateSet.Add(dbEntry);
-						Console.WriteLine($"Inserting: {key} - {entry.name}");
-					}
+					var dbEntry = new DBRPGItemTemplate<ItemClassType, PsobbQuality, Vector3<byte>>(ItemClassType.Guard, 1, entry.name, entry.desc, PsobbQuality.Common);
+					dbEntry.TrySetPropertyValue(nameof(DBRPGItemTemplate<ItemClassType, PsobbQuality, Vector3<byte>>.Id), key);
+					itemTemplateSet.Add(dbEntry);
+					Console.WriteLine($"Inserting: {key} - {entry.name}");
 				}
 
 				await context.SaveChangesAsync(true);
 			}
+		}
+
+		private static async Task LoadWeaponEntriesAsync(DbSet<DBRPGItemTemplate<ItemClassType, PsobbQuality, Vector3<byte>>> itemTemplateSet, DbSet<DBRPGSItemSubClass<ItemClassType>> itemSubclassSet)
+		{
+			//TODO: Remove absolute paths
+			//Load weapon data
+			var weaponData = JsonConvert.DeserializeObject<ItemPMTWeapons>(File.ReadAllText($@"C:\Users\Glader\Documents\Github\Booma.Server\Data\JSON\ItemPMT.Weapons.json"));
+
+			//TODO: We're skipping SubClass 0 even though PSO does have some data in this group (for weapons its nonsense)
+			for (int groupId = 1; groupId < weaponData.weapons.@group.Count; groupId++)
+			{
+				for (int i = 0; i < weaponData.weapons.@group[groupId].list.Count; i++)
+				{
+					var entry = weaponData.weapons.@group[groupId].list[i];
+					var key = CreateItemTemplateKey(ItemClassType.Weapon, groupId, i);
+
+					//If it's already inserted don't reinsert
+					if ((await itemTemplateSet.FindAsync(key)) != null)
+						continue;
+
+					await CreateSubclassIfNeededAsync(ItemClassType.Weapon, itemSubclassSet, groupId);
+
+					var dbEntry = new DBRPGItemTemplate<ItemClassType, PsobbQuality, Vector3<byte>>(ItemClassType.Weapon, groupId, entry.name, entry.desc, PsobbQuality.Common);
+					dbEntry.TrySetPropertyValue(nameof(DBRPGItemTemplate<ItemClassType, PsobbQuality, Vector3<byte>>.Id), key);
+					itemTemplateSet.Add(dbEntry);
+					Console.WriteLine($"Inserting: {key} - {entry.name}");
+				}
+			}
+		}
+
+		private static async Task CreateSubclassIfNeededAsync(ItemClassType @class, DbSet<DBRPGSItemSubClass<ItemClassType>> itemSubclassSet, int subclassId, string name = "TODO")
+		{
+			//First we must consider also that the subclass may not exist.
+			if (await itemSubclassSet.FindAsync(@class, subclassId) == null)
+				itemSubclassSet.Add(new DBRPGSItemSubClass<ItemClassType>(subclassId, @class, $"{name}", name));
+		}
+
+		private static int CreateItemTemplateKey(ItemClassType @class, int groupId, int i)
+		{
+			int shift = (1 * sizeof(byte) * 8);
+
+			//Key: Class:C Group/SubClass:G Index:I 0xCCGGII
+			int key = ((int)@class << shift * 2) + (groupId << shift) + i; 
+			return key;
 		}
 	}
 }
