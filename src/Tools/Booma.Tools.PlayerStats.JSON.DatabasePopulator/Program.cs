07bfb2e14642b69066aa226f822cc90ec054d62c
HelloKitty <andrew.blakely@ymail.com>
2021-12-18 01:25:24 -0600

    Added PlayerStats.JSON.DatabasePopulator tool

---
+++ src/Tools/Booma.Tools.PlayerStats.JSON.DatabasePopulator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Booma.Server.CharacterDataService;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Booma.Tools.PlayerStats.JSON.DatabasePopulator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ServiceCollection services = new ServiceCollection();
+			var mvcBuilder = services.AddMvc();
+			Startup.RegisterGladerRPGSystem(services, mvcBuilder);
+			var provider = services.BuildServiceProvider();
+		}
+	}
+}
